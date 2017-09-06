d39b56618617199ba6a4b53db1f6bd0f91d8e7a6
Bernard Kwok <Bernard.Kwok@autodesk.com>
2017-09-06 19:23:07 -0400

    Fix adsk noise function paths.

--- documents/Libraries/adsk/impl/shadergen/source/glsl/adskCellNoise2d_float.glsl
+++ documents/Libraries/adsk/impl/shadergen/source/glsl/adskCellNoise2d_float.glsl
@@ -1,4 +1,4 @@
-#include "adsk/impl/shadergen/source/glsl/noise_functions.glsl"
+#include "adsk/impl/shadergen/source/glsl/adskNoiseFunctions.glsl"
 
 void adskCellNoise2d_float(vec2 position, float amplitude, float frequency, vec2 distortion, float distortionRatio, out float result)
 {
