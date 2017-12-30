63debcb3f9b819832f5f4a37c2954ea967de81ff
Jonathan Stone <jstone@lucasfilm.com>
2017-12-29 18:32:29 -0800

    Improvements to datatype.py
    This changelist improves the edge-case behavior and documentation of the helper functions in datatype.py.  Specifically, when an input type is unrecognized, the typeToName, valueToString, and stringToValue functions now return None rather than raising an exception.

--- python/MaterialX/datatype.py
+++ python/MaterialX/datatype.py
@@ -31,15 +31,29 @@ _typeToName.update(dict.fromkeys(_stringTypeAliases, 'string'))
 
 #--------------------------------------------------------------------------------
 def typeToName(t):
-    "Return the name of the given MaterialX type."
+    """Return the MaterialX type string associated with the given Python type
+       If the given Python type is not recognized by MaterialX, then None is
+       returned.
+
+       Examples:
+           typeToName(int) -> 'integer'
+           typeToName(mx.Color3) -> 'color3'"""
+
     if t in _typeToName:
         return _typeToName[t]
-    raise TypeError('Unknown MaterialX type: ' + str(t))
+    return None
 
 
 #--------------------------------------------------------------------------------
 def nameToType(name):
-    "Convert a name to its corresponding MaterialX type."
+    """Return the Python type associated with the given MaterialX type string.
+       If the given type string is not recognized by MaterialX, then the Python
+       str type is returned.
+
+       Examples:
+           nameToType('integer') -> int
+           nameToType('color3') -> mx.Color3"""
+
     if name in _nameToType:
         return _nameToType[name]
     return str
@@ -47,15 +61,38 @@ def nameToType(name):
 
 #--------------------------------------------------------------------------------
 def valueToString(value):
-    "Convert a value of MaterialX type to a string."
-    method = globals()['TypedValue_' + typeToName(type(value))].createValue
+    """Convert a Python value of any supported type to its correponding
+       MaterialX value string.  If the Python type of the value is not
+       recognized by MaterialX, then None is returned.
+
+       Examples:
+           valueToString(0.1) -> '0.1'
+           valueToString(mx.Color3(0.1, 0.2, 0.3)) -> '0.1, 0.2, 0.3'"""
+
+    typeString = typeToName(type(value))
+    if not typeString:
+        return None
+    method = globals()['TypedValue_' + typeString].createValue
     return method(value).getValueString()
 
 
 #--------------------------------------------------------------------------------
 def stringToValue(string, t):
-    "Convert a string to a value of MaterialX type."
-    return Value.createValueFromStrings(string, typeToName(t)).getData()
+    """Convert a MaterialX value string and Python type to the corresponding
+       Python value.  If the given conversion cannot be performed, then None
+       is returned.
+
+       Examples:
+           stringToValue('0.1', float) -> 0.1
+           stringToValue('0.1, 0.2, 0.3', mx.Color3) -> mx.Color3(0.1, 0.2, 0.3)"""
+
+    typeString = typeToName(t)
+    if not typeString:
+        return None
+    valueObj = Value.createValueFromStrings(string, typeString)
+    if not valueObj:
+        return None
+    return valueObj.getData()
 
 
 #--------------------------------------------------------------------------------
