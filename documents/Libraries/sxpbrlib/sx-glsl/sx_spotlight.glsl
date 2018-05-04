65dea95928d8806dd118eb2671035faf9e7c95fc
Niklas Harrysson <niklas.harrysson@autodesk.com>
2018-05-04 13:01:37 +0200

    Small adjustment to common light shaders.

--- documents/Libraries/sxpbrlib/sx-glsl/sx_spotlight.glsl
+++ documents/Libraries/sxpbrlib/sx-glsl/sx_spotlight.glsl
@@ -1,11 +1,10 @@
 void sx_spotlight(LightData light, vec3 position, out lightshader result)
 {
     result.direction = light.position - position;
-    float distance = length(result.direction);
-    float attenuation = pow(distance, light.decayRate);
+    float distance = length(result.direction) + M_FLOAT_EPS;
+    float attenuation = pow(distance + 1.0, light.decayRate + M_FLOAT_EPS);
     result.intensity = light.color * light.intensity / attenuation;
     result.direction /= distance;
-
     float low = min(light.innerConeAngle, light.outerConeAngle);
     float high = light.innerConeAngle;
     float cosDir = dot(result.direction, -light.direction);
