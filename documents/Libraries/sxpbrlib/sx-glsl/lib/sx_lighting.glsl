a063caea67309941942dcf8cbbb9b890f8ada7c6
Niklas Harrysson <niklas.harrysson@autodesk.com>
2018-05-17 12:17:43 +0200

    Fixed test failures.

--- documents/Libraries/sxpbrlib/sx-glsl/lib/sx_lighting.glsl
+++ documents/Libraries/sxpbrlib/sx-glsl/lib/sx_lighting.glsl
@@ -1,13 +1,19 @@
+vec2 sx_latlong_projection(vec3 dir)
+{
+    float latitude = -asin(dir.y) * M_PI_INV + 0.5;
+    latitude = clamp(latitude, 0.01, 0.99);
+    float longitude = atan(dir.x, -dir.z) * M_PI_INV * 0.5 + 0.5;
+    return vec2(longitude, latitude);
+}
+
 vec3 sx_latlong_map_lookup(vec3 dir, mat4 transform, sampler2D sampler)
 {
     vec2 res = textureSize(sampler, 0);
     if (res.x > 0)
     {
         vec3 dir = normalize((transform * vec4(dir,0.0)).xyz);
-        float latitude = -asin(dir.y) * M_PI_INV + 0.5;
-        latitude = clamp(latitude, 0.01, 0.99);
-        float longitude = atan2(dir.x, -dir.z) * M_PI_INV * 0.5 + 0.5;
-        return texture2D(sampler, vec2(longitude, latitude)).rgb;
+        vec2 uv = sx_latlong_projection(dir);
+        return texture(sampler, uv).rgb;
     }
     return vec3(0.0);
 }
@@ -23,10 +29,8 @@ vec3 sx_latlong_map_lookup(vec3 dir, mat4 transform, float lodBias, sampler2D sa
         float lod = lodBias * levels;
 
         vec3 dir = normalize((transform * vec4(dir,0.0)).xyz);
-        float latitude = -asin(dir.y) * M_PI_INV + 0.5;
-        latitude = clamp(latitude, 0.01, 0.99);
-        float longitude = atan2(dir.x, -dir.z) * M_PI_INV * 0.5 + 0.5;
-        return textureLod(sampler, vec2(longitude, latitude), lod).rgb;
+        vec2 uv = sx_latlong_projection(dir);
+        return textureLod(sampler, uv, lod).rgb;
     }
     return vec3(0.0);
 }
