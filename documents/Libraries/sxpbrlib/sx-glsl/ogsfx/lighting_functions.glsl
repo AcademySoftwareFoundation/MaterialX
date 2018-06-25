1106d5c65b354d4ec05850a342b4d2bb508d2af4
Jonathan Feldstein <jonathan.feldstein@gmail.com>
2018-06-25 10:30:47 -0400

    Reduces the number of light uniforms from 16 to 8. (#74)

--- documents/Libraries/sxpbrlib/sx-glsl/ogsfx/lighting_functions.glsl
+++ documents/Libraries/sxpbrlib/sx-glsl/ogsfx/lighting_functions.glsl
@@ -16,24 +16,8 @@ int getLightType(int index)
         return u_light5Type;
     else if (index == 6)
         return u_light6Type;
-    else if (index == 7)
-        return u_light7Type;
-    else if (index == 8)
-        return u_light8Type;
-    else if (index == 9)
-        return u_light9Type;
-    else if (index == 10)
-        return u_light10Type;
-    else if (index == 11)
-        return u_light11Type;
-    else if (index == 12)
-        return u_light12Type;
-    else if (index == 13)
-        return u_light13Type;
-    else if (index == 14)
-        return u_light14Type;
     else
-        return u_light15Type;
+        return u_light7Type;
 }
 
 vec3 getLightColor(int index) 
@@ -52,24 +36,8 @@ vec3 getLightColor(int index)
         return u_light5Color;
     else if (index == 6)
         return u_light6Color;
-    else if (index == 7)
-        return u_light7Color;
-    else if (index == 8)
-        return u_light8Color;
-    else if (index == 9)
-        return u_light9Color;
-    else if (index == 10)
-        return u_light10Color;
-    else if (index == 11)
-        return u_light11Color;
-    else if (index == 12)
-        return u_light12Color;
-    else if (index == 13)
-        return u_light13Color;
-    else if (index == 14)
-        return u_light14Color;
     else
-        return u_light15Color;
+        return u_light7Color;
 }
 
 float getLightIntensity(int index)
@@ -88,24 +56,8 @@ float getLightIntensity(int index)
         return u_light5Intensity;
     else if (index == 6)
         return u_light6Intensity;
-    else if (index == 7)
-        return u_light7Intensity;
-    else if (index == 8)
-        return u_light8Intensity;
-    else if (index == 9)
-        return u_light9Intensity;
-    else if (index == 10)
-        return u_light10Intensity;
-    else if (index == 11)
-        return u_light11Intensity;
-    else if (index == 12)
-        return u_light12Intensity;
-    else if (index == 13)
-        return u_light13Intensity;
-    else if (index == 14)
-        return u_light14Intensity;
     else
-        return u_light15Intensity;
+        return u_light7Intensity;
 }
 
 vec3 getLightPos(int index)
@@ -124,24 +76,8 @@ vec3 getLightPos(int index)
         return u_light5Pos;
     else if (index == 6)
         return u_light6Pos;
-    else if (index == 7)
-        return u_light7Pos;
-    else if (index == 8)
-        return u_light8Pos;
-    else if (index == 9)
-        return u_light9Pos;
-    else if (index == 10)
-        return u_light10Pos;
-    else if (index == 11)
-        return u_light11Pos;
-    else if (index == 12)
-        return u_light12Pos;
-    else if (index == 13)
-        return u_light13Pos;
-    else if (index == 14)
-        return u_light14Pos;
     else
-        return u_light15Pos;
+        return u_light7Pos;
 }
 
 vec3 getLightDir(int index) 
@@ -160,24 +96,8 @@ vec3 getLightDir(int index)
         return u_light5Dir;
     else if (index == 6)
         return u_light6Dir;
-    else if (index == 7)
-        return u_light7Dir;
-    else if (index == 8)
-        return u_light8Dir;
-    else if (index == 9)
-        return u_light9Dir;
-    else if (index == 10)
-        return u_light10Dir;
-    else if (index == 11)
-        return u_light11Dir;
-    else if (index == 12)
-        return u_light12Dir;
-    else if (index == 13)
-        return u_light13Dir;
-    else if (index == 14)
-        return u_light14Dir;
     else
-        return u_light15Dir;
+        return u_light7Dir;
 }
 
 float getLightDecayRate(int index)
@@ -196,24 +116,8 @@ float getLightDecayRate(int index)
         return u_light5DecayRate;
     else if (index == 6)
         return u_light6DecayRate;
-    else if (index == 7)
-        return u_light7DecayRate;
-    else if (index == 8)
-        return u_light8DecayRate;
-    else if (index == 9)
-        return u_light9DecayRate;
-    else if (index == 10)
-        return u_light10DecayRate;
-    else if (index == 11)
-        return u_light11DecayRate;
-    else if (index == 12)
-        return u_light12DecayRate;
-    else if (index == 13)
-        return u_light13DecayRate;
-    else if (index == 14)
-        return u_light14DecayRate;
     else
-        return u_light15DecayRate;
+        return u_light7DecayRate;
 }
 
 float getLightConeAngle(int index)
@@ -232,24 +136,8 @@ float getLightConeAngle(int index)
         return cos(u_light5ConeAngle);
     else if (index == 6)
         return cos(u_light6ConeAngle);
-    else if (index == 7)
-        return cos(u_light7ConeAngle);
-    else if (index == 8)
-        return cos(u_light8ConeAngle);
-    else if (index == 9)
-        return cos(u_light9ConeAngle);
-    else if (index == 10)
-        return cos(u_light10ConeAngle);
-    else if (index == 11)
-        return cos(u_light11ConeAngle);
-    else if (index == 12)
-        return cos(u_light12ConeAngle);
-    else if (index == 13)
-        return cos(u_light13ConeAngle);
-    else if (index == 14)
-        return cos(u_light14ConeAngle);
     else
-        return cos(u_light15ConeAngle);
+        return cos(u_light7ConeAngle);
 }
 
 float getLightPenumbraAngle(int index) 
@@ -268,22 +156,6 @@ float getLightPenumbraAngle(int index)
         return cos(u_light5PenumbraAngle);
     else if (index == 6)
         return cos(u_light6PenumbraAngle);
-    else if (index == 7)
-        return cos(u_light7PenumbraAngle);
-    else if (index == 8)
-        return cos(u_light8PenumbraAngle);
-    else if (index == 9)
-        return cos(u_light9PenumbraAngle);
-    else if (index == 10)
-        return cos(u_light10PenumbraAngle);
-    else if (index == 11)
-        return cos(u_light11PenumbraAngle);
-    else if (index == 12)
-        return cos(u_light12PenumbraAngle);
-    else if (index == 13)
-        return cos(u_light13PenumbraAngle);
-    else if (index == 14
-        return cos(u_light14PenumbraAngle);
     else
-        return cos(u_light15PenumbraAngle);
+        return cos(u_light7PenumbraAngle);
 }
