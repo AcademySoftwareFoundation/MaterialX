70dc575dfd17ac9b99d098772e3b18376c787ccc
Jonathan Stone <jstone@lucasfilm.com>
2018-08-25 16:00:09 -0700

    Improvements to XInclude handling in MTLX files (#142)
    - Allow the client to override the callback function used to resolve XInclude references.  This is designed to allow multi-file MaterialX documents to be included in memory-mapped packages such as USDZ.
    - Detect and disallow XInclude cycles.
    - For consistency with XML reading, wrap XML write options within an XmlWriteOptions structure.

--- source/PyMaterialX/PyXmlIo.cpp
+++ source/PyMaterialX/PyXmlIo.cpp
@@ -15,16 +15,22 @@ void bindPyXmlIo(py::module& mod)
 {
     py::class_<mx::XmlReadOptions, mx::CopyOptions>(mod, "XmlReadOptions")
         .def(py::init())
-        .def_readwrite("readXIncludes", &mx::XmlReadOptions::readXIncludes);
+        .def_readwrite("readXIncludeFunction", &mx::XmlReadOptions::readXIncludeFunction)
+        .def_readwrite("parentFilenames", &mx::XmlReadOptions::parentFilenames);
+
+    py::class_<mx::XmlWriteOptions>(mod, "XmlWriteOptions")
+        .def(py::init())
+        .def_readwrite("writeXIncludeEnable", &mx::XmlWriteOptions::writeXIncludeEnable)
+        .def_readwrite("elementPredicate", &mx::XmlWriteOptions::elementPredicate);
 
     mod.def("readFromXmlFileBase", &mx::readFromXmlFile,
         py::arg("doc"), py::arg("filename"), py::arg("searchPath") = mx::EMPTY_STRING, py::arg("readOptions") = (mx::XmlReadOptions*) nullptr);
     mod.def("readFromXmlString", &mx::readFromXmlString,
         py::arg("doc"), py::arg("str"), py::arg("readOptions") = (mx::XmlReadOptions*) nullptr);
     mod.def("writeToXmlFile", mx::writeToXmlFile,
-        py::arg("doc"), py::arg("filename"), py::arg("writeXIncludes") = true, py::arg("predicate") = mx::ElementPredicate());
+        py::arg("doc"), py::arg("filename"), py::arg("writeOptions") = (mx::XmlWriteOptions*) nullptr);
     mod.def("writeToXmlString", mx::writeToXmlString,
-        py::arg("doc"), py::arg("writeXIncludes") = true, py::arg("predicate") = mx::ElementPredicate());
+        py::arg("doc"), py::arg("writeOptions") = nullptr);
     mod.def("prependXInclude", mx::prependXInclude);
 
     py::register_exception<mx::ExceptionParseError>(mod, "ExceptionParseError");
