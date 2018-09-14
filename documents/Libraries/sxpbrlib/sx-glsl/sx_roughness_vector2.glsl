2890605bf36391e711bf576bf00f28b951ce6e5e
Niklas Harrysson <niklas.harrysson@autodesk.com>
2018-09-14 15:44:11 +0200

    Fixed bug with calculation of anisotropic roughness values.

--- documents/Libraries/sxpbrlib/sx-glsl/sx_roughness_vector2.glsl
+++ documents/Libraries/sxpbrlib/sx-glsl/sx_roughness_vector2.glsl
@@ -5,8 +5,7 @@ void sx_roughness_vector2(float roughness, float anisotropy, out vec2 result)
     result.y = roughness_sqr;
     if (anisotropy > 0.0)
     {
-        anisotropy = clamp(anisotropy, 0.0, 0.98);
-        float aspect = sqrt(1.0 - anisotropy);
+        float aspect = sqrt(1.0 - clamp(anisotropy, 0.0, 0.98));
         result.x = min(roughness_sqr / aspect, 1.0);
         result.y = roughness_sqr * aspect;
     }
