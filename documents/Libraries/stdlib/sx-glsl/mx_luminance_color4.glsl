37add992baf1e168ced74d7243bebdd3a1a46ea6
Jonathan Feldstein <jonathan.feldstein@gmail.com>
2018-09-28 11:24:51 -0400

    New adjustment tests and associated test fixes. (#144)

--- documents/Libraries/stdlib/sx-glsl/mx_luminance_color4.glsl
+++ documents/Libraries/stdlib/sx-glsl/mx_luminance_color4.glsl
@@ -1,4 +1,4 @@
-void mx_luminance_color4(vec4 in, vec3 lumacoeffs, out vec4 result)
+void mx_luminance_color4(vec4 _in, vec3 lumacoeffs, out vec4 result)
 {
-    result = vec4(dot(_in.rgb, lumacoeffs), _in.a);
+    result = vec4(vec3(dot(_in.rgb, lumacoeffs)), _in.a);
 }
