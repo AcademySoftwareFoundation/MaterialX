3401a0d1fd4ac87a5d11f928bd9408292f759bf3
niklasharrysson <niklas.harrysson@gmail.com>
2019-01-24 15:36:24 +0100

    Added in missing functions for FIS implementation

--- documents/Libraries/pbrlib/genglsl/lib/mx_math.glsl
+++ documents/Libraries/pbrlib/genglsl/lib/mx_math.glsl
@@ -42,3 +42,15 @@ bool mx_is_tiny(vec3 v)
 {
     return all(lessThan(abs(v), vec3(M_FLOAT_EPS)));
 }
+
+// https://www.graphics.rwth-aachen.de/publication/2/jgt.pdf
+float mx_golden_ratio_sequence(int i)
+{
+    return fract((float(i) + 1.0) * M_GOLDEN_RATIO);
+}
+
+// https://people.irisa.fr/Ricardo.Marques/articles/2013/SF_CGF.pdf
+vec2 mx_spherical_fibonacci(int i, int numSamples)
+{
+    return vec2((float(i) + 0.5) / float(numSamples), mx_golden_ratio_sequence(i));
+}
