aec4d99f5d966938a39d3c3febf45d8b77165afa
Bernard Kwok <Bernard.Kwok@autodesk.com>
2019-02-14 22:47:25 -0500

    Add in support for transformpoint v2*m33 and v3*m44 (#270)

--- documents/Libraries/stdlib/genglsl/mx_transformvector3_matrix44.glsl
+++ documents/Libraries/stdlib/genglsl/mx_transformvector3_matrix44.glsl
@@ -1,6 +1,6 @@
 void transformvector3_matrix44(vec3 val, mat4 transform, out vec3 result)
 {
-  vec4 _val = vec4(val.x, val.y, val.z, 1.0);
+  vec4 _val = vec4(val.x, val.y, val.z, 0.0);
   vec4 res = transform * _val;
   result = res.xyz;
 }
