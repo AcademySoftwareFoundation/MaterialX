f93b32199033a86cf05767bfa7f933ad0aac97cb
Niklas Harrysson <niklas.harrysson@gmail.com>
2019-05-02 17:21:57 +0200

    Added indirect light support for sheen_brdf in GLSL. (#407)

--- libraries/pbrlib/genglsl/mx_sheen_brdf.glsl
+++ libraries/pbrlib/genglsl/mx_sheen_brdf.glsl
@@ -28,25 +28,28 @@ void mx_sheen_brdf_reflection(vec3 L, vec3 V, float weight, vec3 color, float ro
     // https://blog.selfshadow.com/publications/s2013-shading-course/rad/s2013_pbs_rad_notes.pdf
     vec3 fr = D * F / (4.0 * (NdotL + NdotV - NdotL*NdotV));
 
-    // Top layer transmission to attenuate base layer.
-    // We use a LUT of directional albedo for this.
-    float albedo = mx_microfacet_sheen_albedo(NdotV, alpha);
-    float topTrans = clamp(1.0 - albedo * weight, 0.0, 1.0);
+    // Get sheen directional albedo for attenuating base layer
+    // in order to be energy conserving.
+    float albedo = weight * mx_microfacet_sheen_albedo(NdotV, alpha);
 
     // We need to include NdotL from the light integral here
     // as in this case it's not cancelled out by the BRDF denominator.
-    result = fr * NdotL        // Top layer reflection
-           + base * topTrans;  // Base layer reflection attenuated by top layer
-}
-
-void mx_sheen_brdf_transmission(vec3 V, float weight, vec3 color, float roughness, vec3 N, BSDF base, out BSDF result)
-{
-    // Ignore sheen for transparent base layers
-    result = base;
+    result = fr * NdotL             // Top layer reflection
+           + base * (1.0 - albedo); // Base layer reflection attenuated by top layer
 }
 
 void mx_sheen_brdf_indirect(vec3 V, float weight, vec3 color, float roughness, vec3 N, BSDF base, out vec3 result)
 {
-    // TODO: Implement indirect sheen
-    result = base;
+    if (weight <= 0.0)
+    {
+        result = base;
+        return;
+    }
+
+    float NdotV = abs(dot(N,V));
+    float alpha = clamp(roughness, M_FLOAT_EPS, 1.0);
+    float albedo = weight * mx_microfacet_sheen_albedo(NdotV, alpha);
+
+    vec3 Li = mx_environment_irradiance(N);
+    result = Li * color * albedo + base * (1.0 - albedo);
 }
