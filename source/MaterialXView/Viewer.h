57b20ab77965aa6e468c84d1f456252dd2c1a37d
Bernard Kwok <Bernard.Kwok@autodesk.com>
2019-05-23 13:13:51 -0400

    Add in UV topology display option (#439)

--- source/MaterialXView/Viewer.h
+++ source/MaterialXView/Viewer.h
@@ -59,6 +59,9 @@ class Viewer : public ng::Screen
     }
 
   private:
+    void drawScene3D();
+    void drawScene2D();
+
     void setupLights(mx::DocumentPtr doc, const std::string& envRadiancePath, const std::string& envIrradiancePath);
     void initializeDocument(mx::DocumentPtr libraries);
     void reloadDocument();
@@ -85,6 +88,11 @@ class Viewer : public ng::Screen
     void createLoadMeshInterface(Widget* parent, const std::string label);
     void createLoadMaterialsInterface(Widget* parent, const std::string label);
     void createAdvancedSettings(Widget* parent);
+
+    mx::MeshStreamPtr createUvPositionStream(mx::MeshPtr mesh, 
+                                            const std::string& uvStreamName,
+                                            unsigned int index,
+                                            const std::string& positionStreamName);
 
   private:
     ng::Window* _window;
@@ -165,6 +173,13 @@ class Viewer : public ng::Screen
     // Image save
     bool _captureFrame;
     mx::FilePath _captureFrameFileName;
+
+    // UV wireframe drawing
+    bool _drawUVGeometry;
+    MaterialPtr _wireMaterialUV;
+    mx::Vector3 _uvScale;
+    mx::Vector3 _uvTranslation;
+    float _uvZoom;
 };
 
 #endif // MATERIALXVIEW_VIEWER_H
