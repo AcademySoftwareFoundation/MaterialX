e8f1f520778edf2aa5b8e496e1858e2597b70705
niklasharrysson <niklas.harrysson@gmail.com>
2019-07-08 22:52:34 +0200

    Added in library updates from ILM
    (cherry picked from commit bc3f7f8e2bee44e79f781f247b6544b92e0aee6d)

--- libraries/pbrlib/genglsl/mx_artistic_ior.glsl
+++ libraries/pbrlib/genglsl/mx_artistic_ior.glsl
@@ -1,6 +1,6 @@
 #include "pbrlib/genglsl/lib/mx_refraction_index.glsl"
 
-void mx_artistic_ior(vec3 reflectivity, vec3 edgecolor, out vec3 ior, out vec3 extinction)
+void mx_artistic_ior(vec3 reflectivity, vec3 edge_color, out vec3 ior, out vec3 extinction)
 {
-    mx_artistic_to_complex_ior(reflectivity, edgecolor, ior, extinction);
+    mx_artistic_to_complex_ior(reflectivity, edge_color, ior, extinction);
 }
