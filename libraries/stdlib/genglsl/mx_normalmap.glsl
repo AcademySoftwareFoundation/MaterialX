a55190704c303b09bedf49d93c1089d4c2ab25e8
niklasharrysson <niklas.harrysson@gmail.com>
2019-07-10 00:35:07 +0200

    Fixed enum values on normalmap node.
    (cherry picked from commit a3970a507e6b3da470f6b6d39ba0b704f2549be4)

--- libraries/stdlib/genglsl/mx_normalmap.glsl
+++ libraries/stdlib/genglsl/mx_normalmap.glsl
@@ -1,7 +1,7 @@
 void mx_normalmap(vec3 value, int map_space, float normal_scale, vec3 N, vec3 T,  out vec3 result)
 {
     // Tangent space
-    if (map_space == 1)
+    if (map_space == 0)
     {
         vec3 v = value * 2.0 - 1.0;
         vec3 B = normalize(cross(N, T));
