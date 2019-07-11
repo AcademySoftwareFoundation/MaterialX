4ae935ca8a38efe08d42cf26c15a2f6a355d3163
Jonathan Stone <jstone@lucasfilm.com>
2019-07-11 10:32:02 -0700

    Align filename conventions between shading models
    This changelist aligns filename conventions for definition and example documents between the Standard Surface and UsdPreviewSurface shading models.

--- source/MaterialXTest/GenShaderUtil.cpp
+++ source/MaterialXTest/GenShaderUtil.cpp
@@ -473,7 +473,7 @@ void ShaderGeneratorTester::setupDependentLibraries()
 
     // Load shader definitions used in the test suite.
     GenShaderUtil::loadLibrary(mx::FilePath::getCurrentPath() / mx::FilePath("libraries/bxdf/standard_surface.mtlx"), _dependLib);
-    GenShaderUtil::loadLibrary(mx::FilePath::getCurrentPath() / mx::FilePath("libraries/bxdf/usd.mtlx"), _dependLib);
+    GenShaderUtil::loadLibrary(mx::FilePath::getCurrentPath() / mx::FilePath("libraries/bxdf/usd_preview_surface.mtlx"), _dependLib);
 }
 
 void ShaderGeneratorTester::addSkipFiles()
