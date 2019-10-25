f3575903e458fa666f03985d9483c8ff9b6a617e
Jonathan Stone <jstone@lucasfilm.com>
2019-10-25 16:42:58 -0700

    Update formatting

--- libraries/stdlib/genglsl/mx_distance_unit.glsl
+++ libraries/stdlib/genglsl/mx_distance_unit.glsl
@@ -1,15 +1,19 @@
-void mx_distance_unit_float(float _in , int _unit_from, int _unit_to, out float result) {
+void mx_distance_unit_float(float _in, int _unit_from, int _unit_to, out float result)
+{
     result = _in * unit_ratio(_unit_from, _unit_to);
 }
 
-void mx_distance_unit_vector2(vec2 _in , int _unit_from, int _unit_to, out vec2 result) {
+void mx_distance_unit_vector2(vec2 _in, int _unit_from, int _unit_to, out vec2 result)
+{
     result = _in * unit_ratio(_unit_from, _unit_to);
 }
 
-void mx_distance_unit_vector3(vec3 _in , int _unit_from, int _unit_to, out vec3 result) {
+void mx_distance_unit_vector3(vec3 _in, int _unit_from, int _unit_to, out vec3 result)
+{
     result = _in * unit_ratio(_unit_from, _unit_to);
 }
 
-void mx_distance_unit_vector4(vec4 _in , int _unit_from, int _unit_to, out vec4 result) {
+void mx_distance_unit_vector4(vec4 _in, int _unit_from, int _unit_to, out vec4 result)
+{
     result = _in * unit_ratio(_unit_from, _unit_to);
 }
