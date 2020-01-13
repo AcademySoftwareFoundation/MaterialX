5f7ef30e26d882a949e7a19267ce87c4821e17af
Jonathan Stone <jstone@lucasfilm.com>
2020-01-12 20:00:01 -0800

    Extend backdrop support from Document to GraphElement

--- source/PyMaterialX/PyMaterialXCore/PyNode.cpp
+++ source/PyMaterialX/PyMaterialXCore/PyNode.cpp
@@ -26,18 +26,23 @@ void bindPyNode(py::module& mod)
         .def_readonly_static("CATEGORY", &mx::Node::CATEGORY);
 
     py::class_<mx::GraphElement, mx::GraphElementPtr, mx::InterfaceElement>(mod, "GraphElement")
-        .def("_addNode", &mx::NodeGraph::addNode,
+        .def("_addNode", &mx::GraphElement::addNode,
             py::arg("category"), py::arg("name") = mx::EMPTY_STRING, py::arg("type") = mx::DEFAULT_TYPE_STRING)
-        .def("addNodeInstance", &mx::NodeGraph::addNodeInstance,
+        .def("addNodeInstance", &mx::GraphElement::addNodeInstance,
             py::arg("nodeDef"), py::arg("name") = mx::EMPTY_STRING)
-        .def("getNode", &mx::NodeGraph::getNode)
-        .def("getNodes", &mx::NodeGraph::getNodes,
+        .def("getNode", &mx::GraphElement::getNode)
+        .def("getNodes", &mx::GraphElement::getNodes,
             py::arg("category") = mx::EMPTY_STRING)
-        .def("removeNode", &mx::NodeGraph::removeNode)
-        .def("flattenSubgraphs", &mx::NodeGraph::flattenSubgraphs,
+        .def("removeNode", &mx::GraphElement::removeNode)
+        .def("addBackdrop", &mx::GraphElement::addBackdrop,
+            py::arg("name") = mx::EMPTY_STRING)
+        .def("getBackdrop", &mx::GraphElement::getBackdrop)
+        .def("getBackdrops", &mx::GraphElement::getBackdrops)
+        .def("removeBackdrop", &mx::GraphElement::removeBackdrop)
+        .def("flattenSubgraphs", &mx::GraphElement::flattenSubgraphs,
             py::arg("target") = mx::EMPTY_STRING)
-        .def("topologicalSort", &mx::NodeGraph::topologicalSort)
-        .def("asStringDot", &mx::NodeGraph::asStringDot);
+        .def("topologicalSort", &mx::GraphElement::topologicalSort)
+        .def("asStringDot", &mx::GraphElement::asStringDot);
 
     py::class_<mx::NodeGraph, mx::NodeGraphPtr, mx::GraphElement>(mod, "NodeGraph")
         .def("setNodeDef", &mx::NodeGraph::setNodeDef)
