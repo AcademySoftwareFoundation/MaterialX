5fbde394f9d32a6f96ef9775ad9f4fd50489674a
Jonathan Stone <jstone@lucasfilm.com>
2020-04-19 21:39:04 -0700

    Refactor sheen BRDF for clarity
    - Refactor the implementation of the sheen BRDF in GLSL to align with other BxDF nodes.
    - Remove an unused overload of mx_fresnel_schlick.

--- libraries/pbrlib/genglsl/mx_sheen_brdf.glsl
+++ libraries/pbrlib/genglsl/mx_sheen_brdf.glsl
@@ -22,20 +22,16 @@ void mx_sheen_brdf_reflection(vec3 L, vec3 V, float weight, vec3 color, float ro
     float alpha = clamp(roughness, M_FLOAT_EPS, 1.0);
     float D = mx_imageworks_sheen_NDF(NdotH, alpha);
 
-    vec3 F = color * weight;
-
     // Geometry term is skipped and we use a smoother denominator, as in:
     // https://blog.selfshadow.com/publications/s2013-shading-course/rad/s2013_pbs_rad_notes.pdf
-    vec3 fr = D * F / (4.0 * (NdotL + NdotV - NdotL*NdotV));
+    vec3 fr = D * color / (4.0 * (NdotL + NdotV - NdotL*NdotV));
 
-    // Get sheen directional albedo for attenuating base layer
-    // in order to be energy conserving.
-    float albedo = weight * mx_imageworks_sheen_directional_albedo(NdotV, alpha);
+    float dirAlbedo = mx_imageworks_sheen_directional_albedo(NdotV, alpha);
 
     // We need to include NdotL from the light integral here
     // as in this case it's not cancelled out by the BRDF denominator.
-    result = fr * NdotL             // Top layer reflection
-           + base * (1.0 - albedo); // Base layer reflection attenuated by top layer
+    result = fr * NdotL * weight                // Top layer reflection
+           + base * (1.0 - dirAlbedo * weight); // Base layer reflection attenuated by top layer
 }
 
 void mx_sheen_brdf_indirect(vec3 V, float weight, vec3 color, float roughness, vec3 N, BSDF base, out vec3 result)
@@ -48,8 +44,9 @@ void mx_sheen_brdf_indirect(vec3 V, float weight, vec3 color, float roughness, v
 
     float NdotV = abs(dot(N,V));
     float alpha = clamp(roughness, M_FLOAT_EPS, 1.0);
-    float albedo = weight * mx_imageworks_sheen_directional_albedo(NdotV, alpha);
+    float dirAlbedo = mx_imageworks_sheen_directional_albedo(NdotV, alpha);
 
     vec3 Li = mx_environment_irradiance(N);
-    result = Li * color * albedo + base * (1.0 - albedo);
+    result = Li * color * dirAlbedo * weight        // Top layer reflection
+             + base * (1.0 - dirAlbedo * weight);   // Base layer reflection attenuated by top layer
 }
