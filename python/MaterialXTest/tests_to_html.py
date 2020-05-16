6f50a99b6311b98144e013c1b36fa58d106fa7cb
Indy Ray <arelius@gmail.com>
2020-05-15 19:44:02 -0700

    Fix for tests_to_html to operate and generate an html even if it doesn't have both osl -and- glsl

--- python/MaterialXTest/tests_to_html.py
+++ python/MaterialXTest/tests_to_html.py
@@ -4,6 +4,7 @@ import sys
 import os
 import datetime
 import argparse
+import itertools
 
 try:
     # Use pip to install Pillow and Image to enable image diffs
@@ -62,13 +63,12 @@ def main(args=None):
             print ("Number of glsl files " + str(len(glslFiles)) + " does not match number of osl files " +  str(len(oslFiles)) + " in dir: " + subdir)
             print ("GLSL list: " + str(glslFiles))
             print ("OSL files: " + str(oslFiles))
-            continue
-        elif len(glslFiles) > 0 and len(oslFiles) > 0:
+        if len(glslFiles) > 0 or len(oslFiles) > 0:
             fh.write("<h2>" + subdir + ":</h2><br>\n")
             fh.write("<table>\n")
-            for glslFile, oslFile in zip(glslFiles, oslFiles):
-                fullGlslPath = os.path.join(subdir, glslFile)
-                fullOslPath = os.path.join(subdir, oslFile)
+            for glslFile, oslFile in itertools.zip_longest(glslFiles, oslFiles):
+                fullGlslPath = os.path.join(subdir, glslFile) if glslFile else None
+                fullOslPath = os.path.join(subdir, oslFile) if glslFile else None
                 if glslFile and oslFile and DIFF_ENABLED and args.CREATE_DIFF:
                     diffPath = fullGlslPath[0:-8] + "diff.png"
                     createDiff(fullGlslPath, fullOslPath, diffPath)
