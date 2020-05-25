10f779373233c9d2e5db18f25c9518474c69d42b
Jonathan Stone <jstone@lucasfilm.com>
2020-05-25 08:52:44 -0700

    Add documentation for Oren-Nayar diffuse
    This changelist adds documentation for the GLSL implementation of Oren-Nayar diffuse, along with minor updates to function and variable names.

--- libraries/pbrlib/genglsl/lib/mx_microfacet_diffuse.glsl
+++ libraries/pbrlib/genglsl/lib/mx_microfacet_diffuse.glsl
@@ -1,18 +1,19 @@
 #include "pbrlib/genglsl/lib/mx_microfacet.glsl"
 
-float mx_orennayar(vec3 L, vec3 V, vec3 N, float NdotL, float roughness)
+// Based on the OSL implementation of Oren-Nayar diffuse, which is in turn
+// based on https://mimosa-pudica.net/improved-oren-nayar.html.
+float mx_oren_nayar_diffuse(vec3 L, vec3 V, vec3 N, float NdotL, float roughness)
 {
     float LdotV = dot(L, V);
     float NdotV = dot(N, V);
-
-    float t = LdotV - NdotL * NdotV;
-    t = t > 0.0 ? t / max(NdotL, NdotV) : 0.0;
+    float s = LdotV - NdotL * NdotV;
+    float stinv = (s > 0.0f) ? s / max(NdotL, NdotV) : 0.0;
 
     float sigma2 = mx_square(roughness * M_PI);
     float A = 1.0 - 0.5 * (sigma2 / (sigma2 + 0.33));
-    float B = 0.45f * sigma2 / (sigma2 + 0.09);
+    float B = 0.45 * sigma2 / (sigma2 + 0.09);
 
-    return A + B * t;
+    return A + B * stinv;
 }
 
 // https://disney-animation.s3.amazonaws.com/library/s2012_pbs_disney_brdf_notes_v2.pdf
