cb2666fc077084073f92c22c011076e8da75e611
Jonathan Stone <jstone@lucasfilm.com>
2020-08-29 16:53:05 -0700

    Improvements to library loading functions
    - Clarify interfaces and fix Python bindings for loadLibrary and loadLibraries.
    - Leverage library loading functions in genshader.py.

--- python/MaterialXTest/genshader.py
+++ python/MaterialXTest/genshader.py
@@ -3,35 +3,7 @@ import unittest
 
 import MaterialX as mx
 from MaterialX.PyMaterialXGenShader import *
-
 from MaterialX.PyMaterialXGenOsl import OslShaderGenerator, OSL_UNIFORMS, OSL_OUTPUTS
-_fileDir = os.path.dirname(os.path.abspath(__file__))
-
-def _getSubDirectories(libraryPath):
-    return [name for name in os.listdir(libraryPath)
-            if os.path.isdir(os.path.join(libraryPath, name))]
-
-def _getMTLXFilesInDirectory(path):
-    for file in os.listdir(path):
-        if file.endswith(".mtlx"):
-            yield file
-
-_readFromXmlFile = mx.readFromXmlFileBase
-
-def _loadLibrary(file, doc):
-    libDoc = mx.createDocument()
-    _readFromXmlFile(libDoc, file)
-    libDoc.setSourceUri(file)
-    doc.importLibrary(libDoc)
-
-def _loadLibraries(doc, searchPath, libraryPath):
-    librarySubPaths = _getSubDirectories(libraryPath)
-    librarySubPaths.append(libraryPath)
-    for path in librarySubPaths:
-        filenames = _getMTLXFilesInDirectory(os.path.join(libraryPath, path))
-        for filename in filenames:
-            filePath = os.path.join(libraryPath, os.path.join(path, filename))
-            _loadLibrary(filePath, doc)
 
 # Unit tests for GenShader (Python).
 class TestGenShader(unittest.TestCase):
@@ -39,9 +11,10 @@ class TestGenShader(unittest.TestCase):
     def test_ShaderInterface(self):
         doc = mx.createDocument()
 
-        searchPath = os.path.join(_fileDir, "../../libraries")
-        libraryPath = os.path.join(searchPath, "stdlib")
-        _loadLibraries(doc, searchPath, libraryPath)
+        libraryFolders = ["stdlib"]
+        filePath = os.path.dirname(os.path.abspath(__file__))
+        searchPath = os.path.join(filePath, "../../libraries")
+        mx.loadLibraries(libraryFolders, searchPath, doc)
 
         exampleName = u"shader_interface"
 
@@ -73,10 +46,7 @@ class TestGenShader(unittest.TestCase):
 
         shadergen = OslShaderGenerator.create()
         context = GenContext(shadergen)
-        # Add path to find all source code snippets
         context.registerSourceCodeSearchPath(searchPath)
-        # Add path to find OSL include files
-        context.registerSourceCodeSearchPath(os.path.join(libraryPath, "osl"))
 
         # Test complete mode
         context.getOptions().shaderInterfaceType = int(ShaderInterfaceType.SHADER_INTERFACE_COMPLETE);
