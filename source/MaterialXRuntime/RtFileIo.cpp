c77049f719dbd95c15dd30bf9511876aadb99f07
Niklas Harrysson <niklas.harrysson@gmail.com>
2020-09-29 11:53:18 +0200

    Write nodegraph inputs by default. (#978)

--- source/MaterialXRuntime/RtFileIo.cpp
+++ source/MaterialXRuntime/RtFileIo.cpp
@@ -33,8 +33,7 @@ namespace
     // Lists of known metadata which are handled explicitly by import/export.
     static const RtTokenSet nodedefMetadata     = { RtToken("name"), RtToken("type"), RtToken("node") };
     static const RtTokenSet attrMetadata        = { RtToken("name"), RtToken("type"), RtToken("value"), RtToken("nodename"), RtToken("output"), RtToken("channels") };
-    static const RtTokenSet inputMetadata       = { RtToken("name"), RtToken("type"), RtToken("value"), RtToken("nodename"), RtToken("output"), RtToken("channels"),
-                                                    RtToken("doc"), RtToken("uiname"), RtToken("uifolder"), RtToken("uimin"), RtToken("uimax"), RtToken("nodegraph") };
+    static const RtTokenSet inputMetadata       = { RtToken("name"), RtToken("type"), RtToken("value"), RtToken("nodename"), RtToken("output"), RtToken("channels"), RtToken("nodegraph") };
     static const RtTokenSet nodeMetadata        = { RtToken("name"), RtToken("type"), RtToken("node") };
     static const RtTokenSet nodegraphMetadata   = { RtToken("name") };
     static const RtTokenSet genericMetadata     = { RtToken("name"), RtToken("kind") };
@@ -930,7 +929,7 @@ namespace
 
         RtNodeGraph nodegraph(src->hnd());
 
-        if (options && options->writeNodeGraphInputs)
+        if (!options || options->writeNodeGraphInputs)
         {
             // Write inputs/parameters.
             RtObjTypePredicate<RtInput> inputsFilter;
@@ -1278,7 +1277,7 @@ RtReadOptions::RtReadOptions() :
 
 RtWriteOptions::RtWriteOptions() :
     writeIncludes(true),
-    writeNodeGraphInputs(false),
+    writeNodeGraphInputs(true),
     writeDefaultValues(false),
     objectFilter(nullptr),
     metadataFilter(nullptr),
