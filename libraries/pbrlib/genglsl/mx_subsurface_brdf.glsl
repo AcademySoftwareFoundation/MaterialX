787f05e3ff127a569b362a5cab3cbf20287cebac
Jonathan Stone <jstone@lucasfilm.com>
2020-11-06 18:50:40 -0800

    GLSL shading improvements
    - Render back faces with forward-facing normals.
    - Fix artifacts with low roughness values in sheen rendering.

--- libraries/pbrlib/genglsl/mx_subsurface_brdf.glsl
+++ libraries/pbrlib/genglsl/mx_subsurface_brdf.glsl
@@ -8,6 +8,8 @@ void mx_subsurface_brdf_reflection(vec3 L, vec3 V, vec3 P, float occlusion, floa
         return;
     }
 
+    normal = mx_forward_facing_normal(normal, V);
+
     vec3 sss = mx_subsurface_scattering_approx(normal, L, P, color, radius);
     float NdotL = clamp(dot(normal, L), M_FLOAT_EPS, 1.0);
     float visibleOcclusion = 1.0 - NdotL * (1.0 - occlusion);
@@ -21,6 +23,8 @@ void mx_subsurface_brdf_indirect(vec3 V, float weight, vec3 color, vec3 radius, 
         result = BSDF(0.0);
         return;
     }
+
+    normal = mx_forward_facing_normal(normal, V);
 
     // For now, we render indirect subsurface as simple indirect diffuse.
     vec3 Li = mx_environment_irradiance(normal);
