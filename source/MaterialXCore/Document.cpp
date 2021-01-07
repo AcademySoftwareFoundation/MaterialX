8344eb4852f9e6bc0d6fecfdfb595b053318d54d
Jonathan Stone <jstone@lucasfilm.com>
2021-01-06 17:46:50 -0800

    Restore simplified upgrade logic

--- source/MaterialXCore/Document.cpp
+++ source/MaterialXCore/Document.cpp
@@ -55,153 +55,6 @@ NodeDefPtr getShaderNodeDef(ElementPtr shaderRef)
     return NodeDefPtr();
 }
 
-void convertMaterialsToNodes(DocumentPtr doc)
-{
-    for (ElementPtr mat : doc->getChildrenOfType<Element>("material"))
-    {
-        string materialName = mat->getName();
-
-        // Create a temporary name for the material element
-        // so the new node can reuse the existing name.
-        string validName = doc->createValidChildName(materialName + "1");
-        mat->setName(validName);
-
-        // Create a new material node
-        NodePtr materialNode = nullptr;
-
-        // Only include the shader refs explicitly specified on the material instance
-        for (ElementPtr shaderRef : mat->getChildrenOfType<Element>("shaderref"))
-        {
-            // See if shader has been created already.
-            // Should not occur as the shaderref is a uniquely named
-            // child of a uniquely named material element, but the two combined
-            // may have been used for another node instance which not a shader node.
-            string shaderNodeName = materialName + "_" + shaderRef->getName();
-            NodePtr existingShaderNode = doc->getNode(shaderNodeName);
-            if (existingShaderNode)
-            {
-                const string& existingType = existingShaderNode->getType();
-                if (existingType == VOLUME_SHADER_TYPE_STRING ||
-                    existingType == SURFACE_SHADER_TYPE_STRING ||
-                    existingType == DISPLACEMENT_SHADER_TYPE_STRING)
-                {
-                    throw Exception("Shader node already exists: " + shaderNodeName);
-                }
-                else
-                {
-                    shaderNodeName = doc->createValidChildName(shaderNodeName);
-                }
-            }
-
-            // Find the shader type if defined
-            string shaderNodeType = SURFACE_SHADER_TYPE_STRING;
-            NodeDefPtr nodeDef = getShaderNodeDef(shaderRef);
-            if (nodeDef)
-            {
-                shaderNodeType = nodeDef->getType();
-            }
-
-            // Add in a new shader node
-            const string shaderNodeCategory = shaderRef->getAttribute("node");
-            NodePtr shaderNode = doc->addNode(shaderNodeCategory, shaderNodeName, shaderNodeType);
-            shaderNode->setSourceUri(shaderRef->getSourceUri());
-
-            for (ElementPtr child : shaderRef->getChildren())
-            {
-                ElementPtr port = nullptr;
-
-                // Copy over bindinputs as inputs, and bindparams as params
-                if (child->getCategory() == "bindinput")
-                {
-                    port = shaderNode->addInput(child->getName(), child->getAttribute(TypedElement::TYPE_ATTRIBUTE));
-                }
-                else if (child->getCategory() == "bindparam")
-                {
-                    port = shaderNode->addChildOfCategory("parameter", child->getName());
-                    port->setAttribute(TypedElement::TYPE_ATTRIBUTE, child->getAttribute(TypedElement::TYPE_ATTRIBUTE));
-                }
-                else if (child->getCategory() == "bindtoken")
-                {
-                    TokenPtr token = shaderNode->addToken(child->getName());
-                    token->copyContentFrom(child);
-                }
-                if (port)
-                {
-                    // Copy over attributes.
-                    // Note: We preserve inputs which have nodegraph connections,
-                    // as well as top level output connections.
-                    port->copyContentFrom(child);
-                }
-            }
-
-            // Create a new material node if not already created and
-            // add a reference from the material node to the new shader node
-            if (!materialNode)
-            {
-                materialNode = doc->addMaterialNode(materialName, shaderNode);
-                materialNode->setSourceUri(mat->getSourceUri());
-                // Note: Inheritance does not get transfered to the node we do
-                // not perform the following:
-                //      - materialNode->setInheritString(mat->getInheritString());
-            }
-
-            // Create input to replace each shaderref. Use shaderref name as unique
-            // input name.
-            InputPtr shaderInput = materialNode->getInput(shaderNodeType);
-            if (!shaderInput)
-            {
-                shaderInput = materialNode->addInput(shaderNodeType, shaderNodeType);
-                shaderInput->setNodeName(shaderNode->getName());
-            }
-            // Make sure to copy over any target and version information from the shaderref.
-            if (!shaderRef->getTarget().empty())
-            {
-                shaderInput->setTarget(shaderRef->getTarget());
-            }
-            if (!shaderRef->getVersionString().empty())
-            {
-                shaderInput->setVersionString(shaderRef->getVersionString());
-            }
-        }
-
-        // Remove the original material element
-        doc->removeChild(mat->getName());
-    }
-}
-
-void convertParametersToInputs(DocumentPtr doc)
-{
-    // Convert all parameters to be inputs. If needed set them to be "uniform".
-    const StringSet uniformTypes = { FILENAME_TYPE_STRING, STRING_TYPE_STRING };
-    const string PARAMETER_CATEGORY_STRING("parameter");
-    for (ElementPtr e : doc->traverseTree())
-    {
-        InterfaceElementPtr elem = e->asA<InterfaceElement>();
-        if (!elem)
-        {
-            continue;
-        }
-        vector<ElementPtr> children = elem->getChildren();
-        for (ElementPtr child : children)
-        {
-            if (child->getCategory() == PARAMETER_CATEGORY_STRING)
-            {
-                InputPtr newInput = changeChildCategory(elem, child, Input::CATEGORY)->asA<Input>();
-                if (uniformTypes.count(child->getAttribute(TypedElement::TYPE_ATTRIBUTE)))
-                {
-                    newInput->setIsUniform(true);
-                }
-                else
-                {
-                    // TODO: Determine based on usage whether to make
-                    // the input a uniform. 
-                    newInput->setIsUniform(true);
-                }
-            }
-        }
-    }
-}
-
 } // anonymous namespace
 
 //
@@ -1040,8 +893,89 @@ void Document::upgradeVersion()
             }
         }
 
-        // Convert material elements to nodes        
-        convertMaterialsToNodes(asA<Document>());
+        // Convert material elements to nodes
+        for (ElementPtr mat : getChildrenOfType<Element>("material"))
+        {
+            string materialName = mat->getName();
+            NodePtr materialNode = nullptr;
+
+            for (ElementPtr shaderRef : mat->getChildrenOfType<Element>("shaderref"))
+            {
+                // Find the shader type if defined
+                string shaderNodeType = SURFACE_SHADER_TYPE_STRING;
+                NodeDefPtr nodeDef = getShaderNodeDef(shaderRef);
+                if (nodeDef)
+                {
+                    shaderNodeType = nodeDef->getType();
+                }
+
+                // Add the shader node.
+                string shaderNodeName = createValidChildName(shaderRef->getName());
+                string shaderNodeCategory = shaderRef->getAttribute("node");
+                NodePtr shaderNode = addNode(shaderNodeCategory, shaderNodeName, shaderNodeType);
+                shaderNode->setSourceUri(shaderRef->getSourceUri());
+
+                for (ElementPtr child : shaderRef->getChildren())
+                {
+                    ElementPtr port = nullptr;
+
+                    // Copy over bindinputs as inputs, and bindparams as params
+                    if (child->getCategory() == "bindinput")
+                    {
+                        port = shaderNode->addInput(child->getName(), child->getAttribute(TypedElement::TYPE_ATTRIBUTE));
+                    }
+                    else if (child->getCategory() == "bindparam")
+                    {
+                        port = shaderNode->addChildOfCategory("parameter", child->getName());
+                        port->setAttribute(TypedElement::TYPE_ATTRIBUTE, child->getAttribute(TypedElement::TYPE_ATTRIBUTE));
+                    }
+                    else if (child->getCategory() == "bindtoken")
+                    {
+                        TokenPtr token = shaderNode->addToken(child->getName());
+                        token->copyContentFrom(child);
+                    }
+                    if (port)
+                    {
+                        // Copy over attributes.
+                        // Note: We preserve inputs which have nodegraph connections,
+                        // as well as top level output connections.
+                        port->copyContentFrom(child);
+                    }
+                }
+
+                // Create a new material node if not already created and
+                // add a reference from the material node to the new shader node
+                if (!materialNode)
+                {
+                    materialNode = addMaterialNode(createValidName("temp"), shaderNode);
+                    materialNode->setSourceUri(mat->getSourceUri());
+                }
+
+                // Create input to replace each shaderref. Use shaderref name as unique
+                // input name.
+                InputPtr shaderInput = materialNode->getInput(shaderNodeType);
+                if (!shaderInput)
+                {
+                    shaderInput = materialNode->addInput(shaderNodeType, shaderNodeType);
+                    shaderInput->setNodeName(shaderNode->getName());
+                }
+                if (!shaderRef->getTarget().empty())
+                {
+                    shaderInput->setTarget(shaderRef->getTarget());
+                }
+                if (!shaderRef->getVersionString().empty())
+                {
+                    shaderInput->setVersionString(shaderRef->getVersionString());
+                }
+            }
+
+            // Remove the material element and transfer its name to the material node.
+            removeChild(materialName);
+            if (materialNode)
+            {
+                materialNode->setName(materialName);
+            }
+        }
 
         // Update atan2 interface and rotate3d interface
         const string ATAN2 = "atan2";
@@ -1156,7 +1090,20 @@ void Document::upgradeVersion()
         }   
 
         // Convert parameters to inputs, applying uniform markings as needed.
-        convertParametersToInputs(asA<Document>());
+        for (ElementPtr interface : traverseTree())
+        {
+            if (interface->isA<InterfaceElement>())
+            {
+                for (ElementPtr param : interface->getChildrenOfType<Element>("parameter"))
+                {
+                    InputPtr input = updateChildSubclass<Input>(interface, param);
+                    if (interface->isA<NodeDef>())
+                    {
+                        input->setIsUniform(true);
+                    }
+                }
+            }
+        }
 
         // While we are in the process of supporting 1.38. Leave files as 1.37
         minorVersion = 37;
