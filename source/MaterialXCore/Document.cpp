6ac5ad59ab27846d9d8a7095b39800deb901aef8
Bernard Kwok <Bernard.Kwok@autodesk.com>
2021-01-12 11:02:49 -0500

    Make v1.38 upgrade path only mark filename and string parameters as uniforms. (#1077) (#564)

--- source/MaterialXCore/Document.cpp
+++ source/MaterialXCore/Document.cpp
@@ -1099,7 +1099,12 @@ void Document::upgradeVersion()
                     InputPtr input = updateChildSubclass<Input>(interface, param);
                     if (interface->isA<NodeDef>())
                     {
-                        input->setIsUniform(true);
+                        // Only strings and filename types should be set as uniforms.
+                        const string& inputType = input->getType();
+                        if (inputType == FILENAME_TYPE_STRING || inputType == STRING_TYPE_STRING)
+                        {
+                            input->setIsUniform(true);
+                        }
                     }
                 }
             }
