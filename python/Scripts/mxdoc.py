8673945700d779d4acda0d5f25630ba75739abc7
Jonathan Stone <jstone@lucasfilm.com>
2021-01-28 13:20:04 -0800

    Update Python scripts for v1.38
    - Add mxdoc.py and mxvalidate.py to cloud-based tests.
    - Add deprecation warnings for v1.37 Python methods in v1.38, using the original versions of mxdoc.py and mxvalidate.py for testing.
    - Update mxdoc.py and mxvalidate.py for v1.38, simplifying and aligning the Python code with more recent MaterialX scripts.

--- python/Scripts/mxdoc.py
+++ python/Scripts/mxdoc.py
@@ -1,92 +1,45 @@
 #!/usr/bin/env python
 '''
-Output documentation for nodedefs for a given MaterialX document.
+Print markdown documentation for each nodedef in the given document.
 '''
 
-import sys, os, string; os.environ["PYTHONIOENCODING"] = "utf-8"
+import sys, os, argparse
 import MaterialX as mx
 
-def usage():
-    print 'mxdoc.py: Output documentation for MaterialX nodedefs in Markdown format.'
-    print 'Usage:  mxdoc.py <file.mtlx> [<outputfile.md>]'
-    print '- Default output file name is "nodedef_documentation.md"'
-
 HEADERS = ('Name', 'Type', 'Default Value',
-           'UI min', 'UI max',
-           'UI Soft Min', 'UI Soft Max',
-           'UI step', 'UI group',
-           'Description', 'UI Advanced',
-           'Connectable')
+           'Uniform', 'UI min', 'UI max', 'UI Soft Min', 'UI Soft Max', 'UI step', 'UI group', 'UI Advanced')
 
-ATTR_NAMES = ('uimin', 'uimax',
-              'uisoftmin', 'uisoftmax',
-              'uistep', 'uifolder',
-              'doc', 'uiadvanced')
+ATTR_NAMES = ('uniform', 'uimin', 'uimax', 'uisoftmin', 'uisoftmax', 'uistep', 'uifolder', 'uiadvanced')
 
 def main():
-    if len(sys.argv) < 2:
-        usage()
-        sys.exit(0)
-
-    outfilename = 'nodedef_documentation.md'
-    if len(sys.argv) > 2:
-        outfilename = sys.argv[2]
-
-    filename = sys.argv[1]
+    parser = argparse.ArgumentParser(description="Print markdown documentation for each nodedef in the given document.")
+    parser.add_argument(dest="inputFilename", help="Filename of the input MaterialX document.")
+    opts = parser.parse_args()
 
     doc = mx.createDocument()
-
-    # The mx.readFromXmlFile() will fail with
-    #   'MaterialX.ExceptionFileMissing' if the file is not found, or
-    #   'MaterialX.ExceptionParseError' if the document is found but not readable.
     try:
-        mx.readFromXmlFile(doc, filename)
+        mx.readFromXmlFile(doc, opts.inputFilename)
     except mx.ExceptionFileMissing as err:
-        print err
-        return
-    except mx.ExceptionParseError as err:
-        print '%s is not a valid MaterialX file.\n%s' % (filename, err)
-        return
+        print(err)
+        sys.exit(0)
 
-    file = open(outfilename, 'w+')
-
-    nodedefs = doc.getNodeDefs()
-
-    for nd in nodedefs:
-        file.write('- *Nodedef*: %s\n' % nd.getName())
-        file.write('- *Type*: %s\n' % nd.getType())
-        file.write('- *Doc*: %s\n\n' % nd.getAttribute('doc'))
-        file.write('| ' + ' | '.join(HEADERS) + ' |\n')
-        file.write('|' + ' ---- |' * len(HEADERS) + '\n')
+    for nd in doc.getNodeDefs():
+        print('- *Nodedef*: %s' % nd.getName())
+        print('- *Type*: %s' % nd.getType())
+        print('- *Doc*: %s\n' % nd.getAttribute('doc'))
+        print('| ' + ' | '.join(HEADERS) + ' |')
+        print('|' + ' ---- |' * len(HEADERS) + '')
         for inp in nd.getInputs():
             infos = []
             infos.append(inp.getName())
             infos.append(inp.getType())
             val = inp.getValue()
-            if infos[1] == "float":
+            if inp.getType() == "float":
                 val = round(val, 6)
             infos.append(str(val))
             for attrname in ATTR_NAMES:
                 infos.append(inp.getAttribute(attrname))
-            infos.append("true")
-            buf = '| ' + " | ".join(infos) + ' |\n'
-            file.write(buf)
-        for p in nd.getParameters():
-            infos = []
-            infos.append(p.getName())
-            infos.append(p.getType())
-            val = p.getValue()
-            if infos[1] == "float":
-                val = round(val, 6)
-            infos.append(str(val))
-            for attrname in ATTR_NAMES:
-                infos.append(p.getAttribute(attrname))
-            infos.append("false")
-            buf = '| ' + " | ".join(infos) + ' |\n'
-            file.write(buf)
-
-    file.close()
+            print('| ' + " | ".join(infos) + ' |')
 
 if __name__ == '__main__':
     main()
-
