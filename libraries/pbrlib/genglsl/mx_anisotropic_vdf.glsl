a6882925ceea7e1eae1f8655e46f7f8bab98e661
Niklas Harrysson <niklas.harrysson@gmail.com>
2021-05-31 21:25:51 +0200

    TODO comment for volumetric absorption

--- libraries/pbrlib/genglsl/mx_anisotropic_vdf.glsl
+++ libraries/pbrlib/genglsl/mx_anisotropic_vdf.glsl
@@ -2,11 +2,10 @@
 
 void mx_anisotropic_vdf_reflection(vec3 absorption, vec3 scattering, float anisotropy, inout BSDF bsdf)
 {
-    bsdf.throughput *= absorption;
+    // TODO: Add some approximation for volumetric light absorption.
 }
 
 void mx_anisotropic_vdf_transmission(vec3 absorption, vec3 scattering, float anisotropy, inout BSDF bsdf)
 {
-    bsdf.result *= absorption;
-    bsdf.throughput *= absorption;
+    // TODO: Add some approximation for volumetric light absorption.
 }
