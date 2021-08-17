8629678c3796080aaa7d55195fe732337eb57ae5
Jonathan Stone <jstone@lucasfilm.com>
2021-08-16 19:18:10 -0700

    Improvements to shader generation (#677)
    - Update color space names and methods to follow ACES 1.2 conventions (e.g. lin_rec709, g22_rec709), while maintaining compatibility with legacy names (e.g. gamma22).
    - Improve sharing of code and constants for color transforms (e.g. defining mx_srgb_texture_to_lin_rec709 just once per shading language).  Ultimately, we can improve this sharing further by defining color transforms as graphs.
    - Remove unneeded includes in GLSL shader generation (e.g. pbrlib support in pure pattern graphs).

--- libraries/pbrlib/genglsl/lib/mx_microfacet_specular.glsl
+++ libraries/pbrlib/genglsl/lib/mx_microfacet_specular.glsl
@@ -384,3 +384,17 @@ vec3 mx_compute_fresnel(float cosTheta, FresnelData fd)
     else
         return mx_fresnel_airy(cosTheta, fd.ior, fd.extinction, fd.tf_thickness, fd.tf_ior);
 }
+
+vec2 mx_latlong_projection(vec3 dir)
+{
+    float latitude = -asin(dir.y) * M_PI_INV + 0.5;
+    float longitude = atan(dir.x, -dir.z) * M_PI_INV * 0.5 + 0.5;
+    return vec2(longitude, latitude);
+}
+
+vec3 mx_latlong_map_lookup(vec3 dir, mat4 transform, float lod, sampler2D sampler)
+{
+    vec3 envDir = normalize((transform * vec4(dir,0.0)).xyz);
+    vec2 uv = mx_latlong_projection(envDir);
+    return textureLod(sampler, uv, lod).rgb;
+}
