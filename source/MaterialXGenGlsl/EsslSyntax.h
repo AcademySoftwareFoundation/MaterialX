fc72aa6d687c298921703eb512d67a2404e788ec
bernardkwok <bernard.kwok@autodesk.com>
2021-09-29 16:35:54 -0400

    Missed checkin files.

---
+++ source/MaterialXGenGlsl/EsslSyntax.h
@@ -0,0 +1,28 @@
+//
+// TM & (c) 2021 Lucasfilm Entertainment Company Ltd. and Lucasfilm Ltd.
+// All rights reserved.  See LICENSE.txt for license.
+//
+
+#ifndef MATERIALX_ESSLSYNTAX_H
+#define MATERIALX_ESSLSYNTAX_H
+
+/// @file
+/// ESSL syntax class
+
+#include <MaterialXGenGlsl/GlslSyntax.h>
+
+namespace MaterialX
+{
+
+/// Syntax class for ESSL (OpenGL ES Shading Language)
+class MX_GENGLSL_API EsslSyntax : public GlslSyntax
+{
+public:
+    EsslSyntax();
+
+    static SyntaxPtr create() { return std::make_shared<EsslSyntax>(); }
+};
+
+} // namespace MaterialX
+
+#endif
