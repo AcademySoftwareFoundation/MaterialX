3fbc2ea35d6b2704fe30bb641388e201037fdeb1
Niklas Harrysson <niklas.harrysson@gmail.com>
2021-10-26 23:00:07 +0200

    Enabled VDF usage in lama_dielectric

--- libraries/pbrlib/genglsl/mx_anisotropic_vdf.glsl
+++ libraries/pbrlib/genglsl/mx_anisotropic_vdf.glsl
@@ -1,9 +1,4 @@
-void mx_anisotropic_vdf_reflection(vec3 absorption, vec3 scattering, float anisotropy, inout BSDF bsdf)
-{
-    // TODO: Add some approximation for volumetric light absorption.
-}
-
-void mx_anisotropic_vdf_transmission(vec3 absorption, vec3 scattering, float anisotropy, inout BSDF bsdf)
+void mx_anisotropic_vdf(vec3 absorption, vec3 scattering, float anisotropy, inout BSDF bsdf)
 {
     // TODO: Add some approximation for volumetric light absorption.
 }
