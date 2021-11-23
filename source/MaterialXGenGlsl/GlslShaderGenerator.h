9d2d6511e29ae7e39af1e4193db8d7b0d86d499e
Jerry Gamache <jerry.gamache@autodesk.com>
2021-11-23 15:47:37 -0500

    Fix typo and comment.

--- source/MaterialXGenGlsl/GlslShaderGenerator.h
+++ source/MaterialXGenGlsl/GlslShaderGenerator.h
@@ -37,8 +37,8 @@ class MX_GENGLSL_API GlslShaderGenerator : public HwShaderGenerator
     /// Return the version string for the GLSL version this generator is for
     virtual const string& getVersion() const { return VERSION; }
 
-    /// Emit function definitions for all nodes (for 3rd party code with custom shading)
-    virtual void emitLightSampleFunctionDefinitions(const ShaderGraph& graph, GenContext& context, ShaderStage& stage) const;
+    /// Emit function definitions for light sampling code
+    virtual void emitLightSamplerFunctionDefinitions(const ShaderGraph& graph, GenContext& context, ShaderStage& stage) const;
 
     /// Emit a shader variable.
     void emitVariableDeclaration(const ShaderPort* variable, const string& qualifier, GenContext& context, ShaderStage& stage,
