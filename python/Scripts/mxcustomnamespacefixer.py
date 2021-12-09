20e4f7085ffb869ed4b7f9efdce0cfce412dfb16
Jerry Gamache <jerry.gamache@autodesk.com>
2021-12-09 16:04:42 -0500

    Final script used for update.
    Does not fix all files, but the number of manual touchups at
    the end is quite minimal.

--- python/Scripts/mxcustomnamespacefixer.py
+++ python/Scripts/mxcustomnamespacefixer.py
@@ -7,6 +7,28 @@ mx_root = r"."
 clf_exe = r"clang-format"
 git_exe = r"git"
 
+trivial_remove = [b'-namespace MaterialX', b'-{', b'-}', b'-} // namespace MaterialX', b'-} // MaterialX']
+trivial_adds = [b'+MATERIALX_NAMESPACE_BEGIN', b'+', b'+MATERIALX_NAMESPACE_END']
+
+def apply_edits(allLines, source_line, added, removed):
+    validate = False
+    num_open = 0
+    num_close = 0
+    # If the edit is non-trivial, then validate afterwards.
+    if len(added) != len(removed):
+        validate = True
+    for a, d in zip(added,removed):
+        if d in trivial_remove and a in trivial_adds:
+            allLines[source_line - 1] = a[1:].decode("utf-8") + "\n"
+            if a == b'+MATERIALX_NAMESPACE_BEGIN':
+                num_open += 1
+            if a == b'+MATERIALX_NAMESPACE_END':
+                num_close += 1
+        else:
+            validate = True
+        source_line += 1
+    return validate and num_open, num_open, num_close
+
 def fix_namespaces(cur_file):
     seen_open = False
     num_open = 0
@@ -29,14 +51,74 @@ def fix_namespaces(cur_file):
                     continue
                     
                 out_f.write(cur_line)
-    if num_open > 0 and num_open == num_close:
-        os.remove(cur_file)
-        os.rename(cur_file+b".fixed", cur_file)
-        subprocess.run([git_exe, "add", cur_file])
-    else:
+    if num_open == 0 or num_open != num_close:
         os.remove(cur_file+b".fixed")
         subprocess.run([git_exe, "restore", cur_file])
         print("REVERTED:", cur_file, "OPENED:", num_open, "CLOSED:", num_close)
+        return
+        
+    os.remove(cur_file)
+    os.rename(cur_file+b".fixed", cur_file)
+    dr = subprocess.run([git_exe, "diff", "-U0", cur_file], capture_output=True).stdout.split(b"\n")
+
+    # Revert the file (since we do not want clang-format noise in this commit)
+    subprocess.run([git_exe, "restore", cur_file])
+    
+    # Traverse the diff and apply only the namespace edits:
+    with open(cur_file, "rt") as in_f:
+        allLines = in_f.readlines()
+
+    num_open = 0
+    num_close = 0
+        
+    source_line = 0
+    added = []
+    removed = []
+    apply_edit = False
+    validate = False
+    for d in dr:
+        if d.startswith(b"@@"):
+            if apply_edit:
+                ret_validate, ret_open, ret_close = apply_edits(allLines, source_line, added, removed)
+                if ret_validate:
+                    validate = True 
+                num_open += ret_open
+                num_close += ret_close
+            lineno = d.split(b" ")[1].split(b",")[0]
+            source_line = -int(lineno)
+            added = []
+            removed = []
+            continue
+        if d.startswith(b"-"):
+            removed.append(d)
+            continue
+        if d.startswith(b"+"):
+            added.append(d)
+            if d == b'+MATERIALX_NAMESPACE_BEGIN' or d == b'+MATERIALX_NAMESPACE_END':
+                apply_edit = True
+            continue
+
+    if apply_edit:
+        ret_validate, ret_open, ret_close = apply_edits(allLines, source_line, added, removed)
+        if ret_validate:
+            validate = True 
+        num_open += ret_open
+        num_close += ret_close
+
+    if num_open == 0 or num_open != num_close:
+        print("COULD NOT REAPPLY DIFF:", cur_file, "OPENED:", num_open, "CLOSED:", num_close)
+        return
+    
+    with open(cur_file+b".fixed", "wt") as out_f:
+        out_f.writelines(allLines)
+
+    os.remove(cur_file)
+    os.rename(cur_file+b".fixed", cur_file)
+    subprocess.run([git_exe, "add", cur_file])
+
+    if validate:
+        print("RECHECK:", cur_file)
+    
     return 
 
 os.chdir(mx_root)
