0a43f2ff54c37841a2780cafea27a5f5f8091a20
Jonathan Stone <jstone@lucasfilm.com>
2022-01-03 20:13:25 -0800

    Fix script folder location

--- javascript/clean_javascript_win.bat
+++ javascript/clean_javascript_win.bat
@@ -2,4 +2,4 @@ rmdir build /s /q
 rmdir MaterialXTest\_build /s /q
 rmdir MaterialXTest\node_modules /s /q
 rmdir MaterialXView\dist /s /q
-rmdir MaterialXTest\node_modules /s /q
+rmdir MaterialXView\node_modules /s /q
