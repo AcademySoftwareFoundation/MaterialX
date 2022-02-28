b62fa513fd06094b2ab64ffe34e33e87c98107c7
Bernard Kwok <kwokcb@gmail.com>
2022-02-28 12:42:04 -0500

    Add html output support for mxdoc.

--- python/Scripts/mxdoc.py
+++ python/Scripts/mxdoc.py
@@ -12,8 +12,9 @@ HEADERS = ('Name', 'Type', 'Default Value',
 ATTR_NAMES = ('uniform', 'uimin', 'uimax', 'uisoftmin', 'uisoftmax', 'uistep', 'uifolder', 'uiadvanced')
 
 def main():
-    parser = argparse.ArgumentParser(description="Print markdown documentation for each nodedef in the given document.")
+    parser = argparse.ArgumentParser(description="Print documentation for each nodedef in the given document.")
     parser.add_argument(dest="inputFilename", help="Filename of the input MaterialX document.")
+    parser.add_argument('--docType', dest='documentType', default='md', help='Document type. Default is "md" (markdown). "html" is also supported')
     opts = parser.parse_args()
 
     doc = mx.createDocument()
@@ -24,22 +25,53 @@ def main():
         sys.exit(0)
 
     for nd in doc.getNodeDefs():
-        print('- *Nodedef*: %s' % nd.getName())
-        print('- *Type*: %s' % nd.getType())
-        print('- *Doc*: %s\n' % nd.getAttribute('doc'))
-        print('| ' + ' | '.join(HEADERS) + ' |')
-        print('|' + ' ---- |' * len(HEADERS) + '')
-        for inp in nd.getInputs():
-            infos = []
-            infos.append(inp.getName())
-            infos.append(inp.getType())
-            val = inp.getValue()
-            if inp.getType() == "float":
-                val = round(val, 6)
-            infos.append(str(val))
-            for attrname in ATTR_NAMES:
-                infos.append(inp.getAttribute(attrname))
-            print('| ' + " | ".join(infos) + ' |')
+        if opts.documentType == "html":
+            print('<head><style>')
+            print('table, th, td {')
+            print('   border-bottom: 1px solid; border-collapse: collapse; padding: 1%;')
+            print('}')
+            print('</style></head>')
+            print('<ul>')
+            print('<li> <em>Nodedef</em>: %s' % nd.getName())
+            print('<li> <em>Type</em>: %s' % nd.getType())
+            print('<li> <em>Doc</em>: %s\n' % nd.getAttribute('doc'))
+            print('</ul>')
+            print('<table border-collapse=collapse><tr>')
+            for h in HEADERS:
+                print('<th>' + h + '</th>')
+            print('</tr>')
+            for inp in nd.getInputs():
+                print('<tr>')
+                infos = []
+                infos.append(inp.getName())
+                infos.append(inp.getType())
+                val = inp.getValue()
+                if inp.getType() == "float":
+                    val = round(val, 6)
+                infos.append(str(val))
+                for attrname in ATTR_NAMES:
+                    infos.append(inp.getAttribute(attrname))
+                for info in infos:
+                    print('<td>' + info + '</td>')
+                print('</tr>')
+            print('</table>')
+        else:
+            print('- *Nodedef*: %s' % nd.getName())
+            print('- *Type*: %s' % nd.getType())
+            print('- *Doc*: %s\n' % nd.getAttribute('doc'))
+            print('| ' + ' | '.join(HEADERS) + ' |')
+            print('|' + ' ---- |' * len(HEADERS) + '')
+            for inp in nd.getInputs():
+                infos = []
+                infos.append(inp.getName())
+                infos.append(inp.getType())
+                val = inp.getValue()
+                if inp.getType() == "float":
+                    val = round(val, 6)
+                infos.append(str(val))
+                for attrname in ATTR_NAMES:
+                    infos.append(inp.getAttribute(attrname))
+                print('| ' + " | ".join(infos) + ' |')
 
 if __name__ == '__main__':
     main()
