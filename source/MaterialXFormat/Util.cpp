54bb6f44990062b3b574c929389a0a11b15adda9
Bernard Kwok <kwokcb@gmail.com>
2022-03-22 16:25:35 -0400

    Code cleanup.

--- source/MaterialXFormat/Util.cpp
+++ source/MaterialXFormat/Util.cpp
@@ -174,7 +174,6 @@ void flattenFilenames(DocumentPtr doc, const FileSearchPath& searchPath, StringR
                     FilePath testPath = searchPath[i] / resolvedValue;
                     if (testPath.exists())
                     {
-                        //testPath = testPath.removeRelativePathQualifiers();
                         resolvedString = testPath.asString();
                         break;
                     }
