1ac948a75a40fcafcbf2d5b34c3985f2deaa3166
Jonathan Stone <jstone@lucasfilm.com>
2022-05-02 18:12:58 -0700

    Static analysis fixes
    - Remove a duplicated test in UnitConverterRegistry::convertToUnit.
    - Clarify an initialization call in the TextureBaker constructor.
    - Call string::find_first_of with chars rather than strings for performance.
    - Pass file paths by const reference for performance.
    - Remove unused variables.

--- source/MaterialXRenderGlsl/TextureBaker.cpp
+++ source/MaterialXRenderGlsl/TextureBaker.cpp
@@ -81,7 +81,7 @@ TextureBaker::TextureBaker(unsigned int width, unsigned int height, Image::BaseT
     }
 
     // Initialize our base renderer.
-    initialize();
+    GlslRenderer::initialize();
 
     // Initialize our image handler.
     _imageHandler = GLTextureHandler::create(StbImageLoader::create());
