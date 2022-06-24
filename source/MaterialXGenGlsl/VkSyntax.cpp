bf5c404d8f6fb0f5b7ab323501fec08dd3d07e24
Jonathan Stone <jstone@lucasfilm.com>
2022-06-24 07:32:54 -0700

    Simplify VkSyntax source

--- source/MaterialXGenGlsl/VkSyntax.cpp
+++ source/MaterialXGenGlsl/VkSyntax.cpp
@@ -1,14 +1,9 @@
 //
-// TM & (c) 2021 Lucasfilm Entertainment Company Ltd. and Lucasfilm Ltd.
+// TM & (c) 2022 Lucasfilm Entertainment Company Ltd. and Lucasfilm Ltd.
 // All rights reserved.  See LICENSE.txt for license.
 //
 
 #include <MaterialXGenGlsl/VkSyntax.h>
-
-#include <MaterialXGenShader/Shader.h>
-#include <MaterialXGenShader/ShaderGenerator.h>
-
-#include <memory>
 
 MATERIALX_NAMESPACE_BEGIN
 
