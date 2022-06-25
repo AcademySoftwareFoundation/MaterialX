f568bc6ea6ca017538abcab5e399e927e5ffff1a
Jonathan Stone <jstone@lucasfilm.com>
2022-06-24 17:02:41 -0700

    Simplify graph logic in translateShader
    This changelist simplifies the graph selection logic in ShaderTranslator::translateShader, clarifying its intended handling of edge cases.

--- source/MaterialXGenShader/ShaderTranslator.cpp
+++ source/MaterialXGenShader/ShaderTranslator.cpp
@@ -157,14 +157,9 @@ void ShaderTranslator::translateShader(NodePtr shader, const string& destCategor
 
     DocumentPtr doc = shader->getDocument();
     vector<OutputPtr> referencedOutputs = getConnectedOutputs(shader);
-    if (!referencedOutputs.empty() && referencedOutputs[0]->getParent())
-    {
-        _graph = referencedOutputs[0]->getParent()->asA<NodeGraph>();
-    }
-    else
-    {
-        _graph = doc->addNodeGraph();
-    }
+    ElementPtr referencedParent = !referencedOutputs.empty() ? referencedOutputs[0]->getParent() : nullptr;
+    NodeGraphPtr referencedGraph = referencedParent ? referencedParent->asA<NodeGraph>() : nullptr;
+    _graph = referencedGraph ? referencedGraph : doc->addNodeGraph();
 
     string translateNodeString = sourceCategory + "_to_" + destCategory;
     vector<NodeDefPtr> matchingNodeDefs = doc->getMatchingNodeDefs(translateNodeString);
