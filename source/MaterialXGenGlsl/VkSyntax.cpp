7cafa23e753f72dfa5ff4e1da9ba678ea02fb0c9
Ashwin Bhat <1727158+ashwinbhat@users.noreply.github.com>
2022-06-26 16:24:28 -0700

    Vulkan GLSL shader generation (#1002)
    Vulkan Shaders use SPIRV but they are written using a variant of GLSL.
    This variant of GLSL that conforms to Vulkan specification has a few semantic differences. see KHR_vulkan_glsl
    
    This new VkShaderGenerator builds upon existing GLSLShaderGenerator and add Vulkan semantics in the generated GLSL code.
    
    The MaterialX Test code exercises the VkShaderGenerator. In a future change, we will introduce validation and test rendering.

---
+++ source/MaterialXGenGlsl/VkSyntax.cpp
@@ -0,0 +1,14 @@
+//
+// TM & (c) 2022 Lucasfilm Entertainment Company Ltd. and Lucasfilm Ltd.
+// All rights reserved.  See LICENSE.txt for license.
+//
+
+#include <MaterialXGenGlsl/VkSyntax.h>
+
+MATERIALX_NAMESPACE_BEGIN
+
+VkSyntax::VkSyntax()
+{
+}
+
+MATERIALX_NAMESPACE_END
