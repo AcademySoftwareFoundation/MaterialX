c33a2d127e83aa804fac168d8a7cc383d5d5a949
Jonathan Stone <jstone@lucasfilm.com>
2022-07-07 18:56:34 -0700

    Fix initialization warning in GLSL
    This changelist refactors the implementation of mx_surface_transmission in GLSL, addressing an initialization warning when the function is called in WebGL.

--- libraries/pbrlib/genglsl/lib/mx_transmission_refract.glsl
+++ libraries/pbrlib/genglsl/lib/mx_transmission_refract.glsl
@@ -2,14 +2,19 @@
 
 vec3 mx_surface_transmission(vec3 N, vec3 V, vec3 X, vec2 alpha, int distribution, FresnelData fd)
 {
+    vec3 transmission;
     if ($refractionEnv)
     {
-        // Approximate the appearance of dielectric transmission as glossy
+        // Approximate the appearance of surface transmission as glossy
         // environment map refraction, ignoring any scene geometry that might
         // be visible through the surface.
         fd.refraction = true;
-        return mx_environment_radiance(N, V, X, alpha, distribution, fd);
+        transmission = mx_environment_radiance(N, V, X, alpha, distribution, fd);
+    }
+    else
+    {
+        transmission = $refractionColor;
     }
 
-    return $refractionColor;
+    return transmission;
 }
