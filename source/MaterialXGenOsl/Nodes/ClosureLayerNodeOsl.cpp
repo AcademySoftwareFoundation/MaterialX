005822fe7938a8e29c899d955b3f2f6132ea8284
Niklas Harrysson <niklas.harrysson@gmail.com>
2022-07-31 12:34:35 +0200

    Initial changes to support new OSL closures in MaterialXGenOsl

--- source/MaterialXGenOsl/Nodes/ClosureLayerNodeOsl.cpp
+++ source/MaterialXGenOsl/Nodes/ClosureLayerNodeOsl.cpp
@@ -19,7 +19,6 @@ ShaderNodeImplPtr ClosureLayerNodeOsl::create()
 void ClosureLayerNodeOsl::emitFunctionCall(const ShaderNode& _node, GenContext& context, ShaderStage& stage) const
 {
     const ShaderGenerator& shadergen = context.getShaderGenerator();
-    ClosureContext* cct = context.getClosureContext();
 
     ShaderNode& node = const_cast<ShaderNode&>(_node);
 
@@ -38,6 +37,8 @@ void ClosureLayerNodeOsl::emitFunctionCall(const ShaderNode& _node, GenContext& 
     ShaderNode* top = topInput->getConnection()->getNode();
     ShaderNode* base = baseInput->getConnection()->getNode();
 
+#if MATERIALX_OSL_LEGACY_CLOSURES
+
     if (top->hasClassification(ShaderNode::Classification::THINFILM))
     {
         // This is a layer node with thin-film as top layer.
@@ -48,6 +49,8 @@ void ClosureLayerNodeOsl::emitFunctionCall(const ShaderNode& _node, GenContext& 
         {
             throw ExceptionShaderGenError("Thin-film can only be applied to a sibling node, not through a graph interface");
         }
+
+        ClosureContext* cct = context.getClosureContext();
 
         // Set the extra parameters for thin-film.
         ClosureContext::ClosureParams params;
@@ -102,6 +105,42 @@ void ClosureLayerNodeOsl::emitFunctionCall(const ShaderNode& _node, GenContext& 
             shadergen.emitLine(output->getVariable() + ".throughput = " + topResult + ".throughput * " + baseResult + ".throughput", stage);
         }
     }
+
+#else
+
+    if (top->hasClassification(ShaderNode::Classification::THINFILM))
+    {
+        // Make sure the connection to base is a sibling node and not the graph interface.
+        if (base->getParent() != node.getParent())
+        {
+            throw ExceptionShaderGenError("Thin-film can only be applied to a sibling node, not through a graph interface");
+        }
+
+        // TODO: Thin-film is not supported yet.
+        // Emit the function call for base layer but
+        // store the base result in the layer result variable.
+        ScopedSetVariableName setVariable(output->getVariable(), base->getOutput());
+        shadergen.emitFunctionCall(*base, context, stage);
+    }
+    else
+    {
+        // Emit the function call for top and base layer.
+        shadergen.emitFunctionCall(*top, context, stage);
+        shadergen.emitFunctionCall(*base, context, stage);
+
+        // Get the result variables.
+        const string& topResult = topInput->getConnection()->getVariable();
+        const string& baseResult = baseInput->getConnection()->getVariable();
+
+        // Emit the layer closure call.
+        shadergen.emitLineBegin(stage);
+        shadergen.emitOutput(output, true, false, context, stage);
+        shadergen.emitString(" = layer(" + topResult + ", " + baseResult + ")", stage);
+        shadergen.emitLineEnd(stage);
+    }
+
+#endif // MATERIALX_OSL_LEGACY_CLOSURES
+
 }
 
 MATERIALX_NAMESPACE_END
