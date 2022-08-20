249451b079dd6cf94054a82cb880e6f4090386ac
Tellusim <github@tellusim.com>
2022-08-20 08:30:22 -0700

    mx_environment.glsl

--- libraries/pbrlib/genglsl/lib/mx_environment_prefilter.glsl
+++ libraries/pbrlib/genglsl/lib/mx_environment_prefilter.glsl
@@ -1,18 +1,5 @@
 #include "mx_microfacet_specular.glsl"
-
-vec2 mx_latlong_projection(vec3 dir)
-{
-    float latitude = -asin(dir.y) * M_PI_INV + 0.5;
-    float longitude = atan(dir.x, -dir.z) * M_PI_INV * 0.5 + 0.5;
-    return vec2(longitude, latitude);
-}
-
-vec3 mx_latlong_map_lookup(vec3 dir, mat4 transform, float lod, sampler2D envSampler)
-{
-    vec3 envDir = normalize((transform * vec4(dir,0.0)).xyz);
-    vec2 uv = mx_latlong_projection(envDir);
-    return textureLod(envSampler, uv, lod).rgb;
-}
+#include "mx_environment.glsl"
 
 float mx_latlong_compute_lod(float alpha)
 {
