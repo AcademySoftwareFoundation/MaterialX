a1ad6f01b08a63619cf260f50854f5b1945f5bee
Bernard Kwok <kwokcb@gmail.com>
2022-09-14 13:40:27 -0400

    Update for more recent OIIO build. 2.3.17 (currently available version in vcpkg). (https://github.com/OpenImageIO/oiio/releases/tag/v2.3.17.0)

--- source/MaterialXRender/OiioImageLoader.cpp
+++ source/MaterialXRender/OiioImageLoader.cpp
@@ -10,6 +10,8 @@
     #pragma warning(disable: 4100)
     #pragma warning(disable: 4244)
     #pragma warning(disable: 4800)
+    #pragma warning(disable: 4267)
+    #pragma warning(disable: 4456)
 #endif
 
 #include <OpenImageIO/imageio.h>
