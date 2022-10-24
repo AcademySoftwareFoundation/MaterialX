9a65844f3e300f58b00c0220a64b539ad2b81d1e
Rasmus Bonnedal <rasmus.bonnedal@gmail.com>
2022-10-24 11:13:55 +0200

    Add color transform AdobeRGB to rec709
    This commit adds support for transforming AdobeRGB color to rec709. Supported
    gamma is 2.2 (actually 563/256 per AdobeRGB convention) and linear.

--- libraries/stdlib/genglsl/lib/mx_transform_color.glsl
+++ libraries/stdlib/genglsl/lib/mx_transform_color.glsl
@@ -7,3 +7,17 @@ vec3 mx_srgb_texture_to_lin_rec709(vec3 color)
     vec3 powSeg = pow(max(color + vec3(0.055), vec3(0.0)) / 1.055, vec3(2.4));
     return mix(linSeg, powSeg, isAbove);
 }
+
+vec3 mx_lin_adobergb_to_lin_rec709(vec3 color)
+{
+    mat3 adobergb_to_srgb = mat3( 1.39828340e+00, -1.23390452e-07,  4.56148723e-08,
+                                 -3.98283117e-01,  9.99999995e-01, -4.29382905e-02,
+                                  4.42716499e-08,  3.12672428e-08,  1.04293825e+00);
+    return adobergb_to_srgb * color;
+}
+
+vec3 mx_g22_adobergb_to_lin_rec709(vec3 color)
+{
+    vec3 lin_adobergb_color = pow(max(vec3(0.), color), vec3(563 / 256));
+    return mx_lin_adobergb_to_lin_rec709(lin_adobergb_color);
+}
