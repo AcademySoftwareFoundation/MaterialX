b9063d1442ceafb70c1235d62b7ad33777dc2ce5
Jonathan Stone <jstone@lucasfilm.com>
2022-12-04 11:18:51 -0800

    Improvements to color matrices (#1163)
    - Update the AP1_TO_REC709 matrix to match ACES conventions, as recommended by Doug Walker on the OpenColorIO project.
    - Simplify the implementation of AdobeRGB transforms in GLSL and OSL.
    - In GLSL, use constant matrices instead of preprocessor macros, for improved type checking and clarity.
    - In OSL and MDL, clarify the text layout of color matrices.

--- libraries/stdlib/genglsl/mx_lin_adobergb_to_lin_rec709_color4.glsl
+++ libraries/stdlib/genglsl/mx_lin_adobergb_to_lin_rec709_color4.glsl
@@ -2,5 +2,5 @@
 
 void mx_lin_adobergb_to_lin_rec709_color4(vec4 _in, out vec4 result)
 {
-    result = vec4(mx_lin_adobergb_to_lin_rec709(_in.rgb), _in.a);
+    result = vec4(ADOBERGB_TO_REC709 * _in.rgb, _in.a);
 }
