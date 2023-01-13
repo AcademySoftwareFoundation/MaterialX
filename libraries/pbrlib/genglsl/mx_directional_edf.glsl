325a8628bf25067837df88ecbd451beba8428e62
Niklas Harrysson <niklas.harrysson@gmail.com>
2023-01-13 14:46:06 +0100

    Rename new edf node

--- libraries/pbrlib/genglsl/mx_directional_edf.glsl
+++ libraries/pbrlib/genglsl/mx_directional_edf.glsl
@@ -1,6 +1,6 @@
 #include "lib/mx_microfacet.glsl"
 
-void mx_directional_edf(vec3 N, vec3 V, vec3 color0, vec3 color90, float exponent, EDF base, out EDF result)
+void mx_generalized_schlick_edf(vec3 N, vec3 V, vec3 color0, vec3 color90, float exponent, EDF base, out EDF result)
 {
     N = mx_forward_facing_normal(N, V);
     float NdotV = clamp(dot(N, V), M_FLOAT_EPS, 1.0);
