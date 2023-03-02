af98f48163a3c5575005cea0a94a1678fa45eb26
Jonathan Stone <jstone@lucasfilm.com>
2023-03-01 17:46:13 -0800

    Add two-sided refraction option in GLSL
    This changelist adds an option for two-sided refraction in the GLSL approximation of transmission, increasing the visual parity with OSL testrender.
    
    Two-sided refraction approximates the effect of transmitted light rays passing through a double-sided sphere geometry, where the tint of the surface is effectively applied twice.
    
    This new option is disabled by default, and it's enabled manually through LightHandler::setRefractionTwoSided in render comparisons between GLSL and OSL.
    
    Two legacy options that were used only in render comparisons, LightHandler::setRefractionEnv and LightHandler::setRefractionColor, have been removed for clarity.

--- libraries/pbrlib/genglsl/lib/mx_transmission_refract.glsl
+++ libraries/pbrlib/genglsl/lib/mx_transmission_refract.glsl
@@ -1,20 +1,14 @@
 #include "mx_microfacet_specular.glsl"
 
-vec3 mx_surface_transmission(vec3 N, vec3 V, vec3 X, vec2 alpha, int distribution, FresnelData fd)
+vec3 mx_surface_transmission(vec3 N, vec3 V, vec3 X, vec2 alpha, int distribution, FresnelData fd, vec3 tint)
 {
-    vec3 transmission;
-    if ($refractionEnv)
+    // Approximate the appearance of surface transmission as glossy
+    // environment map refraction, ignoring any scene geometry that might
+    // be visible through the surface.
+    fd.refraction = true;
+    if ($refractionTwoSided)
     {
-        // Approximate the appearance of surface transmission as glossy
-        // environment map refraction, ignoring any scene geometry that might
-        // be visible through the surface.
-        fd.refraction = true;
-        transmission = mx_environment_radiance(N, V, X, alpha, distribution, fd);
+        tint = mx_square(tint);
     }
-    else
-    {
-        transmission = $refractionColor;
-    }
-
-    return transmission;
+    return mx_environment_radiance(N, V, X, alpha, distribution, fd) * tint;
 }
