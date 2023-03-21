c832ad95aab2e5df962176a39d761e8521d023a4
Ashwin Bhat <ashwin.bhat@autodesk.com>
2023-03-21 13:11:10 -0700

    Improve mesh comparison

--- source/MaterialXRenderGlsl/GlslProgram.cpp
+++ source/MaterialXRenderGlsl/GlslProgram.cpp
@@ -335,7 +335,7 @@ void GlslProgram::bindMesh(MeshPtr mesh)
         throw ExceptionRenderError("No mesh to bind");
     }
 
-    if (_boundMesh && mesh->getName() != _boundMesh->getName())
+    if (mesh != _boundMesh)
     {
         unbindGeometry();
     }
