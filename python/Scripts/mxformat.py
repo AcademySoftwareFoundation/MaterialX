ce4d923509c6984694d8d29497e27933aede65bd
Bernard Kwok <kwokcb@gmail.com>
2023-05-15 11:33:41 -0400

    Combine mxupdate and mxformat to new mxformat. mxupdate is removed. Revert lama + std surface to preserve "doc" fomatting. Format upgrade documents. Fix mdl mix volume/displacement impl name.

--- python/Scripts/mxformat.py
+++ python/Scripts/mxformat.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 '''
-Format document contents by reading and writing files. 
+Format MaterialX document content by reanding and writing files. Optionally
+upgrade the document version. 
 '''
 
-import os, argparse
+import sys, os, argparse
 import MaterialX as mx
 
 def getFiles(rootPath):
@@ -15,14 +16,15 @@ def getFiles(rootPath):
     return filelist
 
 def main():
-    parser = argparse.ArgumentParser(description="Format document by reading the file and writing it back out.")
-    parser.add_argument(dest="inputFilename", help="Path of file or folder to format")
+    parser = argparse.ArgumentParser(description="Format document by reading the file and writing it back out. Optionally update to the latest version.")
+    parser.add_argument("--yes", dest="yes", action="store_true", help="Proceed without asking for confirmation from the user.")
     parser.add_argument('--checkForChanges', dest='checkForChanges', type=mx.stringToBoolean, default=True, help='Check if a file has changed. Default is True')
-
+    parser.add_argument('--upgradeVersion', dest='upgradeVersion', type=mx.stringToBoolean, default=False, help='Upgrade the document version. Default is False')
+    parser.add_argument(dest="inputFolder", help="An input folder to scan for MaterialX documents.")
     opts = parser.parse_args()
 
     fileList = []
-    rootPath = opts.inputFilename
+    rootPath = opts.inputFolder
     if os.path.isdir(rootPath): 
         fileList = getFiles(rootPath)
     else:
@@ -31,29 +33,52 @@ def main():
     # Preserve version, comments and newlines
     readOptions = mx.XmlReadOptions()
     readOptions.readComments = True
-    readOptions.readNewlines = True
-    readOptions.upgradeVersion = False
+    readOptions.readNewlines = True    
+    readOptions.upgradeVersion = opts.upgradeVersion
 
-    writeOptions = mx.XmlWriteOptions() 
+    validDocs = dict()
+    for filename in fileList:
+        doc = mx.createDocument()
+        try:
+            mx.readFromXmlFile(doc, filename, mx.FileSearchPath(), readOptions)
+            validDocs[filename] = doc
+        except mx.Exception:
+            pass
+
+    if not validDocs:
+        print('No MaterialX documents were found in "%s"' % (opts.inputFolder))
+        return
+
+    print('Found %s MaterialX files in "%s"' % (len(validDocs), opts.inputFolder))
+
+    mxVersion = mx.getVersionIntegers()
+
+    if not opts.yes:
+        question = 'Would you like to update all %i documents in place (y/n)?' % len(validDocs)
+        if opts.upgradeVersion:
+            question = 'Would you like to update all %i documents to MaterialX v%i.%i in place (y/n)?' % (len(validDocs), mxVersion[0], mxVersion[1])
+        answer = input(question)
+        if answer != 'y' and answer != 'Y':
+            return
 
     writeCount = 0
-    for file in fileList:
-        doc = mx.createDocument()              
-        mx.readFromXmlFile(doc, file, mx.FileSearchPath(), readOptions)
-
+    for (filename, doc) in validDocs.items():
         writeFile = True
         if opts.checkForChanges:
-            origString = mx.readFile(file)
+            origString = mx.readFile(filename)
             docString = mx.writeToXmlString(doc)
             if origString == docString:
                 writeFile = False
 
         if writeFile:
             writeCount = writeCount + 1
-            print('- Updated file %s.' % file)
-            mx.writeToXmlFile(doc, file, writeOptions)
+            print('- Updated file %s.' % filename)
+            mx.writeToXmlFile(doc, filename)
 
-    print('- Updated %d files.' % writeCount)
+    if opts.upgradeVersion:
+        print('Updated %i documents to MaterialX v%i.%i' % (writeCount, mxVersion[0], mxVersion[1]))
+    else:
+        print('Updated %i documents ' % writeCount)
 
 if __name__ == '__main__':
     main()
