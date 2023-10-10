fb071c4c812a01b8ac60d4498518fcf52ce9efa8
Frieder Erdmann <frieder.erdmann@gmail.com>
2023-10-10 15:27:57 +0200

    Implemented missing interface, various bug fixes

--- libraries/stdlib/genglsl/mx_creatematrix.glsl
+++ libraries/stdlib/genglsl/mx_creatematrix.glsl
@@ -1,11 +1,19 @@
-void mx_creatematrix_vector3(vec3 in1, vec3 in2, vec3 in3, out mat3 result)
+void mx_creatematrix_vector3_matrix33(vec3 in1, vec3 in2, vec3 in3, out mat3 result)
 {
     result = mat3(in1.x, in1.y, in1.z,
                 in2.x, in2.y, in2.z,
                 in3.x, in3.y, in3.z);
 }
 
-void mx_creatematrix_vector4(vec4 in1, vec4 in2, vec4 in3, vec4 in4, out mat4 result)
+void mx_creatematrix_vector3_matrix44(vec3 in1, vec3 in2, vec3 in3, vec3 in4, out mat4 result)
+{
+    result = mat4(in1.x, in1.y, in1.z, 0.0,
+                  in2.x, in2.y, in2.z, 0.0,
+                  in3.x, in3.y, in3.z, 0.0,
+                  in4.x, in4.y, in4.z, 1.0);
+}
+
+void mx_creatematrix_vector4_matrix44(vec4 in1, vec4 in2, vec4 in3, vec4 in4, out mat4 result)
 {
     result = mat4(in1.x, in1.y, in1.z, in1.w,
                   in2.x, in2.y, in2.z, in2.w,
