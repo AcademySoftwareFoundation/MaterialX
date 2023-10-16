20496d632a7cdc6a3e51efade4e19a5ae40a941b
gareth.morgan <gareth.morgan@autodesk.com>
2023-10-16 10:58:56 -0400

    Better comments

--- source/MaterialXCore/Override.cpp
+++ source/MaterialXCore/Override.cpp
@@ -19,8 +19,13 @@ Override::Override(
     _properties(properties),
     _values(values)
 {
+    // Should we get default input values from document?
     bool getValues = _values.size()==0;
+
+    // Find the document inputs associated with the override properties.
+    // The material inputs are at the start of the traversed tree, so a linear search is most efficent.
     _propertyInputs.resize(_properties.size());
+    // Count how many properties have been found.
     int foundProps = 0;
     for (MaterialX::ElementPtr elem : _doc->traverseTree())
     {
@@ -35,18 +40,24 @@ Override::Override(
                     foundProps++;
                 }
             }
+
+            // Stop the search once all properties have been found.
             if (foundProps == _propertyInputs.size())
                 break;
         }
     }
 
+    // Build the index lookup map and set defaults.
     for (int i = 0; i < properties.size(); i++)
     {
         _indexLookup[properties[i]] = i;
         if (getValues && _propertyInputs[i])
         {
             auto val = _propertyInputs[i]->getValue();
-            _values.push_back(val->copy());
+            if (val)
+                _values.push_back(val->copy());
+            else
+                _values.push_back(Value::createValue(nullptr));
 
         }
     }
--- source/MaterialXCore/Override.h
+++ source/MaterialXCore/Override.h
@@ -7,7 +7,7 @@
 #define MATERIALX_OVERRIDE_H
 
 /// @file
-/// Geometric element subclasses
+/// Material property override classes
 
 #include <MaterialXCore/Export.h>
 #include <MaterialXCore/Value.h>
@@ -22,7 +22,7 @@ class Value;
 using OverridePtr = shared_ptr<class Override>;
 
 /// @class Override
-/// .
+/// Specifies a list of properties (names and values) that override the input values for a document.
 class MX_CORE_API Override
 {
   public:
@@ -32,6 +32,11 @@ class MX_CORE_API Override
         const vector<ValuePtr>& values = {});
     ~Override() { }
 
+    /// Create an override for this document with a list of properties.
+    /// @param doc The document to override.
+    /// @param properties Vector of property names, each one must match the path name of an input in the document.
+    /// @param values Optional vector of values to override the ones in the document, length must match properties.  If not provided values are initialized with defaults from the document.
+    /// @return Pointer to the new override object.
     static OverridePtr create(
         std::shared_ptr<Document> doc,
         const vector<string>& properties,
@@ -40,21 +45,49 @@ class MX_CORE_API Override
         return std::make_shared<Override>(doc, properties, values);
     }
 
-    template<typename T>
-    void setValue(const string& name, const T& val) {
+
+    /// Set the value of the named override property.
+    /// @param name The name of the property to set.
+    /// @param val The value to set, any valid MaterialX type.
+    template <typename T>
+    void setValue(const string& name, const T& val)
+    {
         int idx = getIndex(name);
         _values[idx] = Value::createValue(val);
     }
-    int getIndex(const string& propName) {
-        return _indexLookup[propName];
+
+    /// Get the index of the named property
+    /// @param name The name of the property to lookup.
+    /// @return The index (or -1 if property name is not found.)
+    int getIndex(const string& propName)
+    {
+        auto iter = _indexLookup.find(propName);
+        if (iter == _indexLookup.end())
+            return -1;
+
+        return iter->second;
     }
+
+    /// Get the document for this override.
+    /// @return The document being overriden.
     std::shared_ptr<Document> getDocument() { return _doc; }
 
+    /// Get the document input for the Nth property being overriden.
+    /// @return Pointer to the input within the document.
     std::shared_ptr<Input> getPropertyInput(int n) { return _propertyInputs[n]; }
+
+    /// Get the current value of of the Nth property being overriden.
+    /// @return The current value.
     ValuePtr getValue(int n) { return _values[n]; }
+
+    /// Get the name of the Nth property being overriden.
+    /// @return The full path name of the input associated with the override property.
     string getPropertyName(int n) { return _properties[n]; }
 
+    /// Get the number of properties in this override.
+    /// @return The property count.
     int getPropertyCount() { return int(_properties.size()); }
+
   private:
     std::shared_ptr<Document> _doc;
     vector<string> _properties;
