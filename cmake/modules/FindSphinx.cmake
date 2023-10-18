9d4b4929c8bcf5f647692f078e44e7956949ab18
Stefan Habel <19556655+StefanHabel@users.noreply.github.com>
2023-10-17 23:15:08 -0700

    Sphinx Python Documentation (#1567) - Added MATERIALX_BUILD_PYTHON_DOCS build option.

--- cmake/modules/FindSphinx.cmake
+++ cmake/modules/FindSphinx.cmake
@@ -9,12 +9,12 @@ if(PYTHON3_FOUND)
         "${_PYTHON_EXECUTABLE_DIR}/bin"
         "${_PYTHON_EXECUTABLE_DIR}/Scripts"
     )
+
+    find_program(
+        SPHINX_EXECUTABLE
+        NAMES sphinx-build sphinx-build.exe
+        HINTS ${_SPHINX_SEARCH_PATHS})
+    mark_as_advanced(SPHINX_EXECUTABLE)
+
+    find_package_handle_standard_args(Sphinx DEFAULT_MSG SPHINX_EXECUTABLE)
 endif()
-
-find_program(
-    SPHINX_EXECUTABLE
-    NAMES sphinx-build sphinx-build.exe
-    HINTS ${_SPHINX_SEARCH_PATHS})
-mark_as_advanced(SPHINX_EXECUTABLE)
-
-find_package_handle_standard_args(Sphinx DEFAULT_MSG SPHINX_EXECUTABLE)
