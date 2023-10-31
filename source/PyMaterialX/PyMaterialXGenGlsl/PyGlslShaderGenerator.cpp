40079f192af8597f776edbe45c0b6cdf7edb3a53
Stefan Habel <19556655+StefanHabel@users.noreply.github.com>
2023-10-31 00:04:06 -0700

    Docstrings for PyMaterialXGenGlsl. (#1567)
    Signed-off-by: Stefan Habel <19556655+StefanHabel@users.noreply.github.com>

--- source/PyMaterialX/PyMaterialXGenGlsl/PyGlslShaderGenerator.cpp
+++ source/PyMaterialX/PyMaterialXGenGlsl/PyGlslShaderGenerator.cpp
@@ -22,11 +22,36 @@ namespace mx = MaterialX;
 void bindPyGlslShaderGenerator(py::module& mod)
 {
     py::class_<mx::GlslShaderGenerator, mx::HwShaderGenerator, mx::GlslShaderGeneratorPtr>(mod, "GlslShaderGenerator")
-        .def_static("create", &mx::GlslShaderGenerator::create)
-        .def(py::init<>())
-        .def("generate", &mx::GlslShaderGenerator::generate)
-        .def("getTarget", &mx::GlslShaderGenerator::getTarget)
-        .def("getVersion", &mx::GlslShaderGenerator::getVersion)
+
+        .def_static("create", &mx::GlslShaderGenerator::create,
+                    PYMATERIALX_DOCSTRING(R"docstring(
+    Create an instance of this class.
+)docstring"))
+
+        .def(py::init<>(),
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Initialize an instance of this class.
+)docstring"))
+
+        .def("generate", &mx::GlslShaderGenerator::generate,
+             py::arg("name"),
+             py::arg("element"),
+             py::arg("context"),
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Generate a shader starting from the given `element`, translating the
+    element and all dependencies upstream into shader code.
+)docstring"))
+
+        .def("getTarget", &mx::GlslShaderGenerator::getTarget,
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Return a unique identifier for the target this generator is for.
+)docstring"))
+
+        .def("getVersion", &mx::GlslShaderGenerator::getVersion,
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Return the version string for the GLSL version this generator is for.
+)docstring"))
+
         .doc() = PYMATERIALX_DOCSTRING(R"docstring(
     Base class for GLSL (OpenGL Shading Language) code generation.
     A generator for a specific GLSL target should be derived from this class.
@@ -38,10 +63,38 @@ void bindPyGlslShaderGenerator(py::module& mod)
 void bindPyGlslResourceBindingContext(py::module &mod)
 {
     py::class_<mx::GlslResourceBindingContext, mx::HwResourceBindingContext, mx::GlslResourceBindingContextPtr>(mod, "GlslResourceBindingContext")
-        .def_static("create", &mx::GlslResourceBindingContext::create)
-        .def(py::init<size_t, size_t>())
-        .def("emitDirectives", &mx::GlslResourceBindingContext::emitDirectives)
-        .def("emitResourceBindings", &mx::GlslResourceBindingContext::emitResourceBindings)
+
+        .def_static("create", &mx::GlslResourceBindingContext::create,
+                    py::arg("uniformBindingLocation") = 0,
+                    py::arg("samplerBindingLocation") = 0,
+                    PYMATERIALX_DOCSTRING(R"docstring(
+    Create an instance of this class, initialized using the given binding
+    locations.
+)docstring"))
+
+        .def(py::init<size_t, size_t>(),
+             py::arg("uniformBindingLocation"),
+             py::arg("samplerBindingLocation"),
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Initialize an instance of this class using the given binding locations.
+)docstring"))
+
+        .def("emitDirectives", &mx::GlslResourceBindingContext::emitDirectives,
+             py::arg("context"),
+             py::arg("stage"),
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Emit directives required for binding support.
+)docstring"))
+
+        .def("emitResourceBindings",
+             &mx::GlslResourceBindingContext::emitResourceBindings,
+             py::arg("context"),
+             py::arg("uniforms"),
+             py::arg("stage"),
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Emit uniforms with binding information
+)docstring"))
+
         .doc() = PYMATERIALX_DOCSTRING(R"docstring(
     Class representing a resource binding for GLSL shader resources.
 
@@ -54,11 +107,36 @@ void bindPyGlslResourceBindingContext(py::module &mod)
 void bindPyEsslShaderGenerator(py::module& mod)
 {
     py::class_<mx::EsslShaderGenerator, mx::GlslShaderGenerator, mx::EsslShaderGeneratorPtr>(mod, "EsslShaderGenerator")
-        .def_static("create", &mx::EsslShaderGenerator::create)
-        .def(py::init<>())
-        .def("generate", &mx::EsslShaderGenerator::generate)
-        .def("getTarget", &mx::EsslShaderGenerator::getTarget)
-        .def("getVersion", &mx::EsslShaderGenerator::getVersion)
+
+        .def_static("create", &mx::EsslShaderGenerator::create,
+                    PYMATERIALX_DOCSTRING(R"docstring(
+    Create an instance of this class.
+)docstring"))
+
+        .def(py::init<>(),
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Initialize an instance of this class.
+)docstring"))
+
+        .def("generate", &mx::EsslShaderGenerator::generate,
+             py::arg("name"),
+             py::arg("element"),
+             py::arg("context"),
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Generate a shader starting from the given `element`, translating the
+    element and all dependencies upstream into shader code.
+)docstring"))
+
+        .def("getTarget", &mx::EsslShaderGenerator::getTarget,
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Return a unique identifier for the target this generator is for.
+)docstring"))
+
+        .def("getVersion", &mx::EsslShaderGenerator::getVersion,
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Return the version string for the ESSL version this generator is for.
+)docstring"))
+
         .doc() = PYMATERIALX_DOCSTRING(R"docstring(
     Class implementing an ESSL (OpenGL ES Shading Language) shader generator
 
@@ -71,11 +149,36 @@ void bindPyEsslShaderGenerator(py::module& mod)
 void bindPyVkShaderGenerator(py::module& mod)
 {
     py::class_<mx::VkShaderGenerator, mx::GlslShaderGenerator, mx::VkShaderGeneratorPtr>(mod, "VkShaderGenerator")
-        .def_static("create", &mx::VkShaderGenerator::create)
-        .def(py::init<>())
-        .def("generate", &mx::VkShaderGenerator::generate)
-        .def("getTarget", &mx::VkShaderGenerator::getTarget)
-        .def("getVersion", &mx::VkShaderGenerator::getVersion)
+
+        .def_static("create", &mx::VkShaderGenerator::create,
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Create an instance of this class.
+)docstring"))
+
+        .def(py::init<>(),
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Initialize an instance of this class.
+)docstring"))
+
+        .def("generate", &mx::VkShaderGenerator::generate,
+             py::arg("name"),
+             py::arg("element"),
+             py::arg("context"),
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Generate a shader starting from the given `element`, translating the
+    element and all dependencies upstream into shader code.
+)docstring"))
+
+        .def("getTarget", &mx::VkShaderGenerator::getTarget,
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Return a unique identifier for the target this generator is for.
+)docstring"))
+
+        .def("getVersion", &mx::VkShaderGenerator::getVersion,
+             PYMATERIALX_DOCSTRING(R"docstring(
+    Return the version string for the Vulkan GLSL version this generator is for.
+)docstring"))
+
         .doc() = PYMATERIALX_DOCSTRING(R"docstring(
     Class implementing a Vulkan GLSL shader generator.
 
