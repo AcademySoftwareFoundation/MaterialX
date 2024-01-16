d99db61aedd26dce87f7d5142a1c9904fa2e837f
Jonathan Stone <jstone@lucasfilm.com>
2024-01-15 16:05:42 -0800

    Improvements to noise implementations
    - Leverage node graphs to share the conversion aspects of noise implementations across languages.
    - Simplify noise unit tests to cover only unique implementations.

--- libraries/stdlib/genglsl/mx_noise2d_fa_vector3.glsl
+++
@@ -1,7 +0,0 @@
-#include "lib/mx_noise.glsl"
-
-void mx_noise2d_fa_vector3(float amplitude, float pivot, vec2 texcoord, out vec3 result)
-{
-    vec3 value = mx_perlin_noise_vec3(texcoord);
-    result = value * amplitude + pivot;
-}
