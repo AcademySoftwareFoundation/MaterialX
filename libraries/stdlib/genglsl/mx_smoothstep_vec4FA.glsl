9d2c7ab635dcfcd82a47a8d167b9a350251c7b66
Jonathan Stone <jstone@lucasfilm.com>
2024-01-21 12:43:05 -0800

    Improvements to smoothstep implementations
    - Leverage node graphs to share the conversion aspects of smoothstep implementations across languages.
    - Simplify smoothstep unit tests to cover only unique implementations.

--- libraries/stdlib/genglsl/mx_smoothstep_vec4FA.glsl
+++
@@ -1,9 +0,0 @@
-#include "mx_smoothstep_float.glsl"
-
-void mx_smoothstep_vec4FA(vec4 val, float low, float high, out vec4 result)
-{
-    mx_smoothstep_float(val.x, low, high, result.x);
-    mx_smoothstep_float(val.y, low, high, result.y);
-    mx_smoothstep_float(val.z, low, high, result.z);
-    mx_smoothstep_float(val.w, low, high, result.w);
-}
