9aa26c9e64d4a26760859c93791d4ceedf9f7ab7
Jonathan Stone <jstone@lucasfilm.com>
2024-03-26 16:09:28 -0700

    GLSL improvements to Oren-Nayar diffuse
    - Remove an extra factor of PI in the GLSL computation of Oren-Nayar diffuse, improving its alignment with corresponding implementations in OSL and MDL.
    - Include the required directional albedo factor when computing GLSL environment lighting for Oren-Nayar diffuse.
    - Add an analytic approximation for the directional albedo of Oren-Nayar diffuse, using a rational quadratic fit to Monte Carlo data.
    - Generalize the interfaces of mx_oren_nayar_diffuse and mx_burley_diffuse, enabling their use in tangent-space computations.

--- libraries/pbrlib/genglsl/lib/mx_microfacet_diffuse.glsl
+++ libraries/pbrlib/genglsl/lib/mx_microfacet_diffuse.glsl
@@ -2,28 +2,87 @@
 
 // Based on the OSL implementation of Oren-Nayar diffuse, which is in turn
 // based on https://mimosa-pudica.net/improved-oren-nayar.html.
-float mx_oren_nayar_diffuse(vec3 L, vec3 V, vec3 N, float NdotL, float roughness)
+float mx_oren_nayar_diffuse(float NdotV, float NdotL, float LdotV, float roughness)
 {
-    float LdotV = clamp(dot(L, V), M_FLOAT_EPS, 1.0);
-    float NdotV = clamp(dot(N, V), M_FLOAT_EPS, 1.0);
     float s = LdotV - NdotL * NdotV;
     float stinv = (s > 0.0f) ? s / max(NdotL, NdotV) : 0.0;
 
-    float sigma2 = mx_square(roughness * M_PI);
+    float sigma2 = mx_square(roughness);
     float A = 1.0 - 0.5 * (sigma2 / (sigma2 + 0.33));
     float B = 0.45 * sigma2 / (sigma2 + 0.09);
 
     return A + B * stinv;
 }
 
+float mx_oren_nayar_diffuse_dir_albedo_monte_carlo(float NdotV, float roughness)
+{
+    NdotV = clamp(NdotV, M_FLOAT_EPS, 1.0);
+    vec3 V = vec3(sqrt(1.0f - mx_square(NdotV)), 0, NdotV);
+
+    float radiance = 0.0;
+    const int SAMPLE_COUNT = 64;
+    for (int i = 0; i < SAMPLE_COUNT; i++)
+    {
+        vec2 Xi = mx_spherical_fibonacci(i, SAMPLE_COUNT);
+
+        // Compute the incoming light direction.
+        vec3 L = mx_uniform_sample_hemisphere(Xi);
+        
+        // Compute dot products for this sample.
+        float NdotL = clamp(L.z, M_FLOAT_EPS, 1.0);
+        float LdotV = clamp(dot(L, V), M_FLOAT_EPS, 1.0);
+
+        // Compute diffuse reflectance.
+        float reflectance = mx_oren_nayar_diffuse(NdotV, NdotL, LdotV, roughness);
+
+        // Add the radiance contribution of this sample.
+        //   uniform_pdf = 1 / (2 * PI)
+        //   radiance = (reflectance * NdotL) / (uniform_pdf * PI);
+        radiance += reflectance * NdotL;
+    }
+
+    // Apply global components and normalize.
+    radiance *= 2.0 / float(SAMPLE_COUNT);
+
+    // Return the final directional albedo.
+    return radiance;
+}
+
+float mx_oren_nayar_diffuse_dir_albedo_table_lookup(float NdotV, float roughness)
+{
+#if DIRECTIONAL_ALBEDO_METHOD == 1
+    if (textureSize($albedoTable, 0).x > 1)
+    {
+        return texture($albedoTable, vec2(NdotV, roughness)).b;
+    }
+#endif
+    return 0.0;
+}
+
+// Rational quadratic fit to Monte Carlo data for Oren-Nayar directional albedo.
+float mx_oren_nayar_diffuse_dir_albedo_analytic(float NdotV, float roughness)
+{
+    vec2 r = vec2(1.0, 1.0) +
+             vec2(-0.4297, -0.6076) * roughness +
+             vec2(-0.7632, -0.4993) * NdotV * roughness +
+             vec2(1.4385, 2.0315) * mx_square(roughness);
+    return r.x / r.y;
+}
+
+float mx_oren_nayar_diffuse_dir_albedo(float NdotV, float roughness)
+{
+#if DIRECTIONAL_ALBEDO_METHOD == 2
+    float dirAlbedo = mx_oren_nayar_diffuse_dir_albedo_monte_carlo(NdotV, roughness);
+#else
+    float dirAlbedo = mx_oren_nayar_diffuse_dir_albedo_analytic(NdotV, roughness);
+#endif
+    return clamp(dirAlbedo, 0.0, 1.0);
+}
+
 // https://media.disneyanimation.com/uploads/production/publication_asset/48/asset/s2012_pbs_disney_brdf_notes_v3.pdf
 // Section 5.3
-float mx_burley_diffuse(vec3 L, vec3 V, vec3 N, float NdotL, float roughness)
+float mx_burley_diffuse(float NdotV, float NdotL, float LdotH, float roughness)
 {
-    vec3 H = normalize(L + V);
-    float LdotH = clamp(dot(L, H), M_FLOAT_EPS, 1.0);
-    float NdotV = clamp(dot(N, V), M_FLOAT_EPS, 1.0);
-
     float F90 = 0.5 + (2.0 * roughness * mx_square(LdotH));
     float refL = mx_fresnel_schlick(NdotL, 1.0, F90);
     float refV = mx_fresnel_schlick(NdotV, 1.0, F90);
