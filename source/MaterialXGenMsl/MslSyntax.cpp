AcademySoftwareFoundation/MaterialX

commit 2007ebf3ff887e098c8b3240036da3d438c3ec17

Remove unused BSDF variables in MSL
This changelist removes two unused variables from the BSDF structure in MSL, aligning this structure with its equivalent in GLSL.

source/MaterialXGenMsl/MslSyntax.cpp
@@ -263,10 +263,10 @@ MslSyntax::MslSyntax()
         Type::BSDF,
         std::make_shared<AggregateTypeSyntax>(
             "BSDF",
-            "BSDF{float3(0.0),float3(1.0), 0.0, 0.0}",
+            "BSDF{float3(0.0),float3(1.0)}",
             EMPTY_STRING,
             EMPTY_STRING,
-            "struct BSDF { float3 response; float3 throughput; float thickness; float ior; };"));
+            "struct BSDF { float3 response; float3 throughput; };"));
 
     registerTypeSyntax(
         Type::EDF,

commit 83f589a07589aa2ed581843f25daaea467332467

Fix syntax bug introduced in 2007ebf

source/MaterialXGenMsl/MslSyntax.cpp
@@ -281,7 +281,7 @@ MslSyntax::MslSyntax()
         Type::VDF,
         std::make_shared<AggregateTypeSyntax>(
             "BSDF",
-            "BSDF{float3(0.0),float3(1.0), 0.0, 0.0}",
+            "BSDF{float3(0.0),float3(1.0)}",
             EMPTY_STRING));
 
     registerTypeSyntax(