f98abc434e0e502f5299cf6ea076a410d4d13614
Niklas Harrysson <niklas.harrysson@gmail.com>
2024-06-17 02:50:53 +0200

    Remove unused thin_surface node (#1890)
    This changelist removes the `thin_surface` node. The specification for thin-walled surfaces was changed in 1.39, making this node deprecated. 
    
    The node was never implemented across multiple targets (MDL is the only exception), and we've never heard of anyone trying to use it. So no upgrade logic is added at this point. Should anyone need an upgrade path we can add that later.

--- source/MaterialXTest/MaterialXGenGlsl/GenGlsl.cpp
+++ source/MaterialXTest/MaterialXGenGlsl/GenGlsl.cpp
@@ -81,7 +81,7 @@ TEST_CASE("GenShader: GLSL Implementation Check", "[genglsl]")
 
     mx::StringSet generatorSkipNodeTypes;
     mx::StringSet generatorSkipNodeDefs;
-    GenShaderUtil::checkImplementations(context, generatorSkipNodeTypes, generatorSkipNodeDefs, 31);
+    GenShaderUtil::checkImplementations(context, generatorSkipNodeTypes, generatorSkipNodeDefs, 30);
 }
 
 TEST_CASE("GenShader: GLSL Unique Names", "[genglsl]")
