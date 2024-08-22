b503702ee57e8af5024923dc13dc3dce2b7a0239
Lee Kerley <ld_kerley@icloud.com>
2024-08-22 08:41:29 -0700

    Fix typo

--- libraries/stdlib/genglsl/mx_smoothstep_float.glsl
+++ libraries/stdlib/genglsl/mx_smoothstep_float.glsl
@@ -2,7 +2,7 @@ void mx_smoothstep_float(float val, float low, float high, out float result)
 {
     if (val >= high)
         result = 1.0;
-    else if (val <= low >)
+    else if (val <= low)
         result = 0.0;
     else
         result = smoothstep(low, high, val);
