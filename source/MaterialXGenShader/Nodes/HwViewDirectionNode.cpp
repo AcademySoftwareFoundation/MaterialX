594e0124de61d5fa34bfb6dad4f14b0ad350e67b
edobrowo <ethan.dobrowolski@gmail.com>
2024-09-28 17:02:31 -0400

    Compute view direction in world space for both paths before transforming

--- source/MaterialXGenShader/Nodes/HwViewDirectionNode.cpp
+++ source/MaterialXGenShader/Nodes/HwViewDirectionNode.cpp
@@ -16,22 +16,18 @@ ShaderNodeImplPtr HwViewDirectionNode::create()
 
 void HwViewDirectionNode::createVariables(const ShaderNode& node, GenContext&, Shader& shader) const
 {
-    ShaderStage vs = shader.getStage(Stage::VERTEX);
-    ShaderStage ps = shader.getStage(Stage::PIXEL);
+    ShaderStage& vs = shader.getStage(Stage::VERTEX);
+    ShaderStage& ps = shader.getStage(Stage::PIXEL);
 
     addStageInput(HW::VERTEX_INPUTS, Type::VECTOR3, HW::T_IN_POSITION, vs);
 
     const ShaderInput* spaceInput = node.getInput(SPACE);
     const int space = spaceInput ? spaceInput->getValue()->asA<int>() : OBJECT_SPACE;
-    if (space == WORLD_SPACE)
+
+    addStageUniform(HW::PRIVATE_UNIFORMS, Type::VECTOR3, HW::T_VIEW_POSITION, vs);
+    addStageConnector(HW::VERTEX_DATA, Type::VECTOR3, HW::T_VIEW_DIRECTION, vs, ps);
+    if (space == OBJECT_SPACE || space == MODEL_SPACE)
     {
-        addStageConnector(HW::VERTEX_DATA, Type::VECTOR3, HW::T_POSITION_WORLD, vs, ps);
-        addStageUniform(HW::PRIVATE_UNIFORMS, Type::VECTOR3, HW::T_VIEW_POSITION, ps);
-    }
-    else
-    {
-        addStageConnector(HW::VERTEX_DATA, Type::VECTOR3, HW::T_POSITION_OBJECT, vs, ps);
-        addStageUniform(HW::PRIVATE_UNIFORMS, Type::VECTOR3, HW::T_VIEW_POSITION, ps);
         addStageUniform(HW::PRIVATE_UNIFORMS, Type::MATRIX44, HW::T_WORLD_INVERSE_TRANSPOSE_MATRIX, ps);
     }
 }
@@ -47,23 +43,11 @@ void HwViewDirectionNode::emitFunctionCall(const ShaderNode& node, GenContext& c
     {
         VariableBlock& vertexData = stage.getOutputBlock(HW::VERTEX_DATA);
         const string prefix = shadergen.getVertexDataPrefix(vertexData);
-        if (space == WORLD_SPACE)
+        ShaderPort* view_direction = vertexData[HW::T_VIEW_DIRECTION];
+        if (!view_direction->isEmitted())
         {
-            ShaderPort* position = vertexData[HW::T_POSITION_WORLD];
-            if (!position->isEmitted())
-            {
-                position->setEmitted();
-                shadergen.emitLine(prefix + position->getVariable() + " = hPositionWorld.xyz", stage);
-            }
-        }
-        else
-        {
-            ShaderPort* position = vertexData[HW::T_POSITION_OBJECT];
-            if (!position->isEmitted())
-            {
-                position->setEmitted();
-                shadergen.emitLine(prefix + position->getVariable() + " = " + HW::T_IN_POSITION, stage);
-            }
+            view_direction->setEmitted();
+            shadergen.emitLine(prefix + view_direction->getVariable() + " = hPositionWorld.xyz - " + HW::T_VIEW_POSITION, stage);
         }
     }
 
@@ -73,15 +57,14 @@ void HwViewDirectionNode::emitFunctionCall(const ShaderNode& node, GenContext& c
         const string prefix = shadergen.getVertexDataPrefix(vertexData);
         shadergen.emitLineBegin(stage);
         shadergen.emitOutput(node.getOutput(), true, false, context, stage);
+        ShaderPort* view_direction = vertexData[HW::T_VIEW_DIRECTION];
         if (space == WORLD_SPACE)
         {
-            ShaderPort* position = vertexData[HW::T_POSITION_WORLD];
-            shadergen.emitString(" = normalize(" + prefix + position->getVariable() + " - " + HW::T_VIEW_POSITION + ")", stage);
+            shadergen.emitString(" = normalize(" + prefix + view_direction->getVariable() + ")", stage);
         }
         else
         {
-            ShaderPort* position = vertexData[HW::T_POSITION_OBJECT];
-            shadergen.emitString(" = normalize(" + prefix + position->getVariable() + " - (" + HW::T_WORLD_INVERSE_TRANSPOSE_MATRIX + " * vec4(" + HW::T_VIEW_POSITION + ", 0.0)).xyz)", stage);
+            shadergen.emitString(" = normalize((" + HW::T_WORLD_INVERSE_TRANSPOSE_MATRIX + " * vec4(" + prefix + view_direction->getVariable() + ", 0.0)).xyz)", stage);
         }
         shadergen.emitLineEnd(stage);
     }
