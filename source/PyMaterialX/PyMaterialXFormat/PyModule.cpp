dabf56865e0a74bba71f2394d9e541caf4383421
Jonathan Stone <jstone@lucasfilm.com>
2024-10-03 07:50:47 -0700

    Broaden scope of serialization for future work
    Signed-off-by: Jonathan Stone <jstone@lucasfilm.com>

--- source/PyMaterialX/PyMaterialXFormat/PyModule.cpp
+++ source/PyMaterialX/PyMaterialXFormat/PyModule.cpp
@@ -13,7 +13,7 @@ void bindPyUtil(py::module& mod);
 
 PYBIND11_MODULE(PyMaterialXFormat, mod)
 {
-    mod.doc() = "Cross-platform support for file and search paths, and XML serialization.";
+    mod.doc() = "Cross-platform support for file paths, search paths, and serialization.";
 
     // PyMaterialXFormat depends on types defined in PyMaterialXCore
     PYMATERIALX_IMPORT_MODULE(PyMaterialXCore);
