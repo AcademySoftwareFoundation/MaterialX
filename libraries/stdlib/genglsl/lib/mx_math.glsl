1b412b24dc9594aef0c46a548b99b6074fe15775
Jonathan Stone <jstone@lucasfilm.com>
2024-10-09 16:24:49 -0700

    Minor refactoring for clarity and compactness
    Signed-off-by: Jonathan Stone <jstone@lucasfilm.com>

--- libraries/stdlib/genglsl/lib/mx_math.glsl
+++ libraries/stdlib/genglsl/lib/mx_math.glsl
@@ -1,20 +1,13 @@
-// This file, and the corresponding mx_math.msl provide functions with a common interface, to allow
-// code to be shared between GLSL and MSL.
-// Guideline : prefix any functions with `mx_` to ensure no conflict with code that other shader
-// generators (like HdStorm) might create.
-
 #define M_FLOAT_EPS 1e-8
 
 float mx_square(float x)
 {
     return x*x;
 }
-
 vec2 mx_square(vec2 x)
 {
     return x*x;
 }
-
 vec3 mx_square(vec3 x)
 {
     return x*x;
@@ -38,11 +31,91 @@ float mx_radians(float degree)
     return radians(degree);
 }
 
-// GLSL uses atan(x,y) and MSL uses atan2(x,y)
-// follow the GLSL convention but prefix with mx_ to ensure consistent function
-// names between languages.
+float mx_sin(float x)
+{
+    return sin(x);
+}
+vec2 mx_sin(vec2 x)
+{
+    return sin(x);
+}
+vec3 mx_sin(vec3 x)
+{
+    return sin(x);
+}
+vec4 mx_sin(vec4 x)
+{
+    return sin(x);
+}
 
-// atan()
+float mx_cos(float x)
+{
+    return cos(x);
+}
+vec2 mx_cos(vec2 x)
+{
+    return cos(x);
+}
+vec3 mx_cos(vec3 x)
+{
+    return cos(x);
+}
+vec4 mx_cos(vec4 x)
+{
+    return cos(x);
+}
+
+float mx_tan(float x)
+{
+    return tan(x);
+}
+vec2 mx_tan(vec2 x)
+{
+    return tan(x);
+}
+vec3 mx_tan(vec3 x)
+{
+    return tan(x);
+}
+vec4 mx_tan(vec4 x)
+{
+    return tan(x);
+}
+
+float mx_asin(float x)
+{
+    return asin(x);
+}
+vec2 mx_asin(vec2 x)
+{
+    return asin(x);
+}
+vec3 mx_asin(vec3 x)
+{
+    return asin(x);
+}
+vec4 mx_asin(vec4 x)
+{
+    return asin(x);
+}
+
+float mx_acos(float x)
+{
+    return acos(x);
+}
+vec2 mx_acos(vec2 x)
+{
+    return acos(x);
+}
+vec3 mx_acos(vec3 x)
+{
+    return acos(x);
+}
+vec4 mx_acos(vec4 x)
+{
+    return acos(x);
+}
+
 float mx_atan(float y_over_x)
 {
     return atan(y_over_x);
@@ -62,97 +135,4 @@ vec3 mx_atan(vec3 y, vec3 x)
 vec4 mx_atan(vec4 y, vec4 x)
 {
     return atan(y, x);
-}
-
-// the trigonometric functions below are one-to-one with their original counterparts
-// in GLSL and MSL - just here to ensure we mx_ prefix families of functions
-
-// cos()
-float mx_cos(float x)
-{
-    return cos(x);
-}
-vec2 mx_cos(vec2 x)
-{
-    return cos(x);
-}
-vec3 mx_cos(vec3 x)
-{
-    return cos(x);
-}
-vec4 mx_cos(vec4 x)
-{
-    return cos(x);
-}
-
-// sin()
-float mx_sin(float x)
-{
-    return sin(x);
-}
-vec2 mx_sin(vec2 x)
-{
-    return sin(x);
-}
-vec3 mx_sin(vec3 x)
-{
-    return sin(x);
-}
-vec4 mx_sin(vec4 x)
-{
-    return sin(x);
-}
-
-// tan()
-float mx_tan(float x)
-{
-    return tan(x);
-}
-vec2 mx_tan(vec2 x)
-{
-    return tan(x);
-}
-vec3 mx_tan(vec3 x)
-{
-    return tan(x);
-}
-vec4 mx_tan(vec4 x)
-{
-    return tan(x);
-}
-
-// acos()
-float mx_acos(float x)
-{
-    return acos(x);
-}
-vec2 mx_acos(vec2 x)
-{
-    return acos(x);
-}
-vec3 mx_acos(vec3 x)
-{
-    return acos(x);
-}
-vec4 mx_acos(vec4 x)
-{
-    return acos(x);
-}
-
-// asin()
-float mx_asin(float x)
-{
-    return asin(x);
-}
-vec2 mx_asin(vec2 x)
-{
-    return asin(x);
-}
-vec3 mx_asin(vec3 x)
-{
-    return asin(x);
-}
-vec4 mx_asin(vec4 x)
-{
-    return asin(x);
 }
