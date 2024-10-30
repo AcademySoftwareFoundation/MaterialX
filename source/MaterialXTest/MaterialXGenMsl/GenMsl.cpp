cf852cf7a07a4724b5cee187661516e76a21e62f
Masuo Suzuki <153872239+msuzuki-nvidia@users.noreply.github.com>
2024-10-29 18:24:31 -0700

    another attempt

--- source/MaterialXTest/MaterialXGenMsl/GenMsl.cpp
+++ source/MaterialXTest/MaterialXGenMsl/GenMsl.cpp
@@ -92,7 +92,7 @@ TEST_CASE("GenShader: MSL Implementation Check", "[genmsl]")
     generatorSkipNodeDefs.insert("ND_hextiledimage_color4");
     generatorSkipNodeDefs.insert("ND_hextilednormalmap_vector3");
 
-    GenShaderUtil::checkImplementations(context, generatorSkipNodeTypes, generatorSkipNodeDefs, 34);
+    GenShaderUtil::checkImplementations(context, generatorSkipNodeTypes, generatorSkipNodeDefs, 33);
 }
 
 TEST_CASE("GenShader: MSL Unique Names", "[genmsl]")
