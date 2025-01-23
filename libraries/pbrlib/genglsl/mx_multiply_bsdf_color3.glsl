c0dbaddc16aa4ad6431c0fbba01a9a204d248d41
Jonathan Stone <jstone@lucasfilm.com>
2025-01-23 12:00:29 -0800

    Minor naming update
    Signed-off-by: Jonathan Stone <jstone@lucasfilm.com>

--- libraries/pbrlib/genglsl/mx_multiply_bsdf_color3.glsl
+++ libraries/pbrlib/genglsl/mx_multiply_bsdf_color3.glsl
@@ -1,8 +1,8 @@
 void mx_multiply_bsdf_color3(BSDF in1, vec3 in2, out BSDF result)
 {
-    vec3 weight = clamp(in2, 0.0, 1.0);
-    result.response = in1.response * weight;
-    result.throughput = in1.throughput * weight;
+    vec3 tint = clamp(in2, 0.0, 1.0);
+    result.response = in1.response * tint;
+    result.throughput = in1.throughput * tint;
 }
 
 void mx_multiply_bsdf_color3_reflection(vec3 _unused1, vec3 _unused2, vec3 _unused3, float _unused4, BSDF in1, vec3 in2, out BSDF result)
