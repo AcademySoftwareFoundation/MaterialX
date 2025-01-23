29cdc3679505261a9c8e06da4a1fbe14ea285358
Lee Kerley <ld_kerley@icloud.com>
2025-01-22 22:30:48 -0800

    Clean up

--- libraries/pbrlib/genglsl/mx_multiply_edf_color3.glsl
+++ libraries/pbrlib/genglsl/mx_multiply_edf_color3.glsl
@@ -2,18 +2,3 @@ void mx_multiply_edf_color3(vec3 _unused1, vec3 _unused2, EDF in1, vec3 in2, out
 {
     result = in1 * in2;
 }
-//
-//void mx_multiply_edf_color3_reflection(vec3 _unused1, vec3 _unused2, vec3 _unused3, EDF in1, vec3 in2, out EDF result)
-//{
-//    mx_multiply_edf_color3(in1, in2, result);
-//}
-//
-//void mx_multiply_edf_color3_transmission(vec3 _unused1, EDF in1, vec3 in2, out EDF result)
-//{
-//    mx_multiply_edf_color3(in1, in2, result);
-//}
-//
-//void mx_multiply_edf_color3_indirect(vec3 _unused1, EDF in1, vec3 in2, out EDF result)
-//{
-//    mx_multiply_edf_color3(in1, in2, result);
-//}
