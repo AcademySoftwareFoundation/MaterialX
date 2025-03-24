34f430e195a26a125397062e61c9ac6f50a39319
tdavidovicNV <tdavidovic@nvidia.com>
2025-03-24 17:36:36 +0100

    Remove unnecessary includes in TextureBaker implementations (#2300)
    GLSL and Msl TextureBaker implementations are including files that are not necessary.

--- source/MaterialXRenderGlsl/TextureBaker.cpp
+++ source/MaterialXRenderGlsl/TextureBaker.cpp
@@ -5,14 +5,6 @@
 
 #include <MaterialXRenderGlsl/TextureBaker.h>
 
-#include <MaterialXRender/OiioImageLoader.h>
-#include <MaterialXRender/StbImageLoader.h>
-#include <MaterialXRender/Util.h>
-
-#include <MaterialXGenShader/DefaultColorManagementSystem.h>
-
-#include <MaterialXFormat/XmlIo.h>
-
 MATERIALX_NAMESPACE_BEGIN
 TextureBakerGlsl::TextureBakerGlsl(unsigned int width, unsigned int height, Image::BaseType baseType) :
     TextureBaker<GlslRenderer, GlslShaderGenerator>(width, height, baseType, true)
