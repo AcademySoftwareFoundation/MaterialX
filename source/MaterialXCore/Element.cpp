655bd404e4a6ede25457a636d9b9a8de96704536
Bernard Kwok <kwokcb@gmail.com>
2025-05-03 17:32:00 -0400

    Fix token handling in functional nodegraphs (#2363)
    ## Issue
    
    Tokens on `nodedef`s are not checked when resolving inputs on nodes in a functional nodegraph. 
    
    ## Change
    
    Patch token search to check `nodedef` tokens when applying token resolution

--- source/MaterialXCore/Element.cpp
+++ source/MaterialXCore/Element.cpp
@@ -873,6 +873,28 @@ void StringResolver::addTokenSubstitutions(ConstElementPtr element)
                     setFilenameSubstitution(key, value);
                 }
             }
+
+            // This is a functional nodegraph. Get the corresponding nodedef and
+            // check for tokens on it.
+            ConstNodeGraphPtr nodegraph = parent->asA<NodeGraph>();
+            if (nodegraph)
+            {
+                NodeDefPtr nodedef = nodegraph->getNodeDef();
+                if (nodedef)
+                {
+                    tokens = nodedef->getActiveTokens();
+                    for (auto token : tokens)
+                    {
+                        string key = DELIMITER_PREFIX + token->getName() + DELIMITER_POSTFIX;
+                        string value = token->getResolvedValueString();
+                        if (!_filenameMap.count(key))
+                        {
+                            setFilenameSubstitution(key, value);
+                        }
+                    }
+                }
+            }
+
         }
         parent = parent->getParent();
     }
