2e0e2b3dd77bc9e58c33c450736d36104f4efe56
Jonathan Stone <jstone@lucasfilm.com>
2025-05-03 18:38:52 -0700

    Fix GLSL implementation of layer node
    This changelist fixes a typo in the GLSL implementation of the `layer` node, where the throughput term was computed via addition rather than multiplication.
    
    For more straightforward validation, the `layer_bsdf.mtlx` example has been updated so that the throughput computation in `layer` influences its rendered result.

--- libraries/pbrlib/genglsl/mx_layer_bsdf.glsl
+++ libraries/pbrlib/genglsl/mx_layer_bsdf.glsl
@@ -3,5 +3,5 @@
 void mx_layer_bsdf(ClosureData closureData, BSDF top, BSDF base, out BSDF result)
 {
     result.response = top.response + base.response * top.throughput;
-    result.throughput = top.throughput + base.throughput;
+    result.throughput = top.throughput * base.throughput;
 }
