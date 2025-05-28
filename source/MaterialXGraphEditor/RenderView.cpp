c0b9b9f0022b9e238ce3b8c7ec3b895fea621b70
Warren Moore <39937547+warrenm@users.noreply.github.com>
2025-05-27 20:27:45 -0700

    Fix frame uniform binding in Metal rendering (#2415)
    This PR adjusts how the frame uniform is bound in the Metal renderers of the Graph Editor and Viewer so that the frame index is correctly propagated to shaders.

--- source/MaterialXGraphEditor/RenderView.cpp
+++ source/MaterialXGraphEditor/RenderView.cpp
@@ -808,10 +808,7 @@ void RenderView::renderFrame()
         {
             material->getProgram()->bindUniform(mx::HW::ALPHA_THRESHOLD, mx::Value::createValue(0.99f));
         }
-        if (material->getProgram()->hasUniform(mx::HW::FRAME))
-        {
-            material->getProgram()->bindUniform(mx::HW::FRAME, mx::Value::createValue(static_cast<float>(_frame)));
-        }
+        material->getProgram()->bindTimeAndFrame(1.0f, static_cast<float>(_frame));
         material->bindViewInformation(_viewCamera);
         material->bindLighting(_lightHandler, _imageHandler, shadowState);
         material->bindImages(_imageHandler, _searchPath);
@@ -839,6 +836,7 @@ void RenderView::renderFrame()
             {
                 material->getProgram()->bindUniform(mx::HW::ALPHA_THRESHOLD, mx::Value::createValue(0.001f));
             }
+            material->getProgram()->bindTimeAndFrame(1.0f, static_cast<float>(_frame));
             material->bindViewInformation(_viewCamera);
             material->bindLighting(_lightHandler, _imageHandler, shadowState);
             material->bindImages(_imageHandler, _searchPath);
