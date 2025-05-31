5da6019fe66e8fcae93033e0d3a872dfccfa1a08
Jonathan Stone <jstone@lucasfilm.com>
2025-05-31 11:12:12 -0700

    Guard against missing texcoords
    This changelist adds a guard against missing texture coordinates, falling back to a computation that uses screen-space gradients alone.

--- libraries/stdlib/genglsl/mx_heighttonormal_vector3.glsl
+++ libraries/stdlib/genglsl/mx_heighttonormal_vector3.glsl
@@ -3,7 +3,8 @@ void mx_heighttonormal_vector3(float height, float scale, vec2 texcoord, out vec
     // Compute the gradient of the heightfield signal with respect
     // to the input texture coordinates.
     vec2 dHdS = vec2(-dFdx(height), dFdy(height)) * scale;
-    vec2 dTdS = (abs(dFdx(texcoord)) + abs(dFdy(texcoord))) * 16.0;
+    vec2 dTdS = abs(dFdx(texcoord)) + abs(dFdy(texcoord));
+    dTdS = (dot(dTdS, dTdS) == 0.0) ? vec2(1.0) : dTdS * 16.0;
     vec2 dHdT = dHdS / dTdS;
 
     // Convert the gradient to a normal and encode for storage.
