ec2627dc62ba40b1edc930849f697bd069086ee7
Jonathan Stone <jstone@lucasfilm.com>
2025-05-30 20:44:20 -0700

    Adjust shader constants
    This changelist makes adjustments to shader constants in GLSL and OSL, providing better visual parity with traditional Sobel sample filtering.

--- libraries/stdlib/genglsl/mx_heighttonormal_vector3.glsl
+++ libraries/stdlib/genglsl/mx_heighttonormal_vector3.glsl
@@ -3,12 +3,8 @@ void mx_heighttonormal_vector3(float height, float scale, vec2 texcoord, out vec
     // Compute the gradient of the heightfield signal with respect
     // to the input texture coordinates.
     vec2 dHdS = vec2(-dFdx(height), dFdy(height)) * scale;
-    vec2 dTdS = abs(dFdx(texcoord)) + abs(dFdy(texcoord));
+    vec2 dTdS = (abs(dFdx(texcoord)) + abs(dFdy(texcoord))) * 16.0;
     vec2 dHdT = dHdS / dTdS;
-
-    // Apply a scale factor to match discrete heightfield sampling.
-    float DISCRETE_SCALE_FACTOR = 0.04;
-    dHdT *= DISCRETE_SCALE_FACTOR;
 
     // Convert the gradient to a normal and encode for storage.
     vec3 n = normalize(vec3(dHdT.x, dHdT.y, 1.0));
