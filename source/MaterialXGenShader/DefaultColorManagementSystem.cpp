4ac0999f7dc9daa2e0b3727f9fa6313ef796739f
Jerry Gamache <jerry.gamache@autodesk.com>
2025-08-04 14:38:05 -0400

    Align MaterialX to Color Interop recommendations
    For more details, see the [Texture asset color space](https://github.com/AcademySoftwareFoundation/ColorInterop/blob/main/Recommendations/01_TextureAssetColorSpaces/TextureAssetColorSpaces.md) recommendations.
    
    - All names are now based on color interop short names
    - Deprecated names are now labeled as such
    - Unit tests still covers all names even older legacy ones
    - Introduced lin_ap0, lin_rec2020, and srgb_ap1 color spaces

--- source/MaterialXGenShader/DefaultColorManagementSystem.cpp
+++ source/MaterialXGenShader/DefaultColorManagementSystem.cpp
@@ -13,6 +13,7 @@ namespace
 {
 
 const string CMS_NAME = "default_cms";
+const string INTEROP_SUFFIX = "_scene";
 
 // Remap from legacy color space names to their ACES 1.2 equivalents.
 const StringMap COLOR_SPACE_REMAP =
@@ -20,7 +21,13 @@ const StringMap COLOR_SPACE_REMAP =
     { "gamma18", "g18_rec709" },
     { "gamma22", "g22_rec709" },
     { "gamma24", "rec709_display" },
-    { "lin_ap1", "acescg" }
+
+    // Remap from 1.38 names to color interop equivalents (minus _scene suffix)
+    { "acescg", "lin_ap1" },
+    { "srgb_displayp3", "srgb_p3d65" },
+    { "lin_displayp3", "lin_p3d65" },
+    { "srgb_texture", "srgb_rec709" },
+    { "adobergb", "g22_adobergb" },
 };
 
 } // anonymous namespace
@@ -53,6 +60,23 @@ NodeDefPtr DefaultColorManagementSystem::getNodeDef(const ColorSpaceTransform& t
 
     string sourceSpace = COLOR_SPACE_REMAP.count(transform.sourceSpace) ? COLOR_SPACE_REMAP.at(transform.sourceSpace) : transform.sourceSpace;
     string targetSpace = COLOR_SPACE_REMAP.count(transform.targetSpace) ? COLOR_SPACE_REMAP.at(transform.targetSpace) : transform.targetSpace;
+
+    // Color interop short names systematically end in "_scene". We can truncate to keep as
+    // many names as possible from the 1.38 era.
+    if (stringEndsWith(sourceSpace, INTEROP_SUFFIX))
+    {
+        sourceSpace = sourceSpace.substr(0, sourceSpace.size() - INTEROP_SUFFIX.size());
+    }
+    if (stringEndsWith(targetSpace, INTEROP_SUFFIX))
+    {
+        targetSpace = targetSpace.substr(0, targetSpace.size() - INTEROP_SUFFIX.size());
+    }
+
+    if (sourceSpace == targetSpace)
+    {
+        return _document->getNodeDef("ND_dot_" + transform.type.getName());
+    }
+
     string nodeName = sourceSpace + "_to_" + targetSpace;
 
     for (NodeDefPtr nodeDef : _document->getMatchingNodeDefs(nodeName))
