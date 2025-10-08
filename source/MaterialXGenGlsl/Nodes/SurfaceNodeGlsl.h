31a16de36ab256850600ea903a859f1055a610b8
Lee Kerley <ld_kerley@icloud.com>
2025-10-08 16:20:01 -0700

    Restore two files being included by the current OpenUSD build that were removed, along with the necessary alias to allow OpenUSD to build.

---
+++ source/MaterialXGenGlsl/Nodes/SurfaceNodeGlsl.h
@@ -0,0 +1,21 @@
+//
+// Created by Lee Kerley on 10/8/25.
+//
+
+#ifndef MATERIALX_SURFACENODEGLSL_H
+#define MATERIALX_SURFACENODEGLSL_H
+
+#include <MaterialXGenShader/Nodes/HwSurfaceNode.h>
+
+MATERIALX_NAMESPACE_BEGIN
+
+// This alias from SurfaceNodeGlsl to HwSurfaceNode is
+// a backwards compatibility affordance for OpenUSD
+// The old SurfaceNodeGlsl class was removed in MatX 1.39.5
+// once that becomes the minimum version for MaterialX
+// in OpenUSD we can remove this.
+using SurfaceNodeGlsl = HwSurfaceNode;
+
+MATERIALX_NAMESPACE_END
+
+#endif // MATERIALX_SURFACENODEGLSL_H
