5aa88fcfd535a288f2953c1d93f428ab4e7d6a71
Lee Kerley <ld_kerley@icloud.com>
2025-10-17 13:45:23 -0700

    fix typo in windows code

--- source/MaterialXFormat/File.cpp
+++ source/MaterialXFormat/File.cpp
@@ -185,8 +185,8 @@ FilePathVec FilePath::getFilesInDirectory(const string& extension) const
 
 #if defined(_WIN32)
     WIN32_FIND_DATAA fd;
-    FilePath query = extension.empty() ? *this : (*this / ("*." + extension)
-    HANDLE hFind = FindFirstFileA((*this / wildcard).asString().c_str(), &fd);
+    FilePath query = extension.empty() ? *this : (*this / ("*." + extension);
+    HANDLE hFind = FindFirstFileA(query.asString().c_str(), &fd);
     if (hFind != INVALID_HANDLE_VALUE)
     {
         do
