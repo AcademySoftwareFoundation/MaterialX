AcademySoftwareFoundation/MaterialX

commit 5818e06e79091f4fbb4219da8951fb996f1c4ce3

Unify remaining GLSL and MSL nodes as HW nodes (#2555)
The diff between the Msl and Glsl nodes showed that the differences were almost exclusively in the types (e.g., `vec3` vs `float3`), which are already handled by `#define`s in the Msl codepath. I've unified all the nodes as HwKindNode in `MaterialXGenShader/Nodes` where all the other shared nodes are.

I have introduced a `makeClosureData` call with `$closureDataConstructor` implementation.

The reason is that Msl requires constructing structs via initializer lists like so:
```
{CLOSURE_TYPE_REFLECTION, L, V, N, P, occlusion}
```

While GLSL only allows this in versions since 4.6 (or 4.2, see https://www.khronos.org/opengl/wiki/Data_Type_(GLSL)#Initializer_lists )

Please note that `LightCompoundNodeMsl` on main is generating this code:
```
shadergen.emitLine("ClosureData closureData = ClosureData(CLOSURE_TYPE_EMISSION, float3(0), -L, light.direction, float3(0), 0)", stage);
```
which is invalid.

The other significant change is Msl condition for emitting transmission ignored VDF
```
	if (bsdf->hasClassification(ShaderNode::Classification::BSDF_T)) {
		shadergen.emitLine("ClosureData closureData = {CLOSURE_TYPE_TRANSMISSION, L, V, N, P, occlusion}", stage);
		shadergen.emitFunctionCall(*bsdf, context, stage);
```
changed to (code emitted for VDF, not just transmission):
```
	if (bsdf->hasClassification(ShaderNode::Classification::BSDF_T) || bsdf->hasClassification(ShaderNode::Classification::VDF)) {
		shadergen.emitLine("ClosureData closureData = makeClosureData(CLOSURE_TYPE_TRANSMISSION, L, V, N, P, occlusion)", stage);
		shadergen.emitFunctionCall(*bsdf, context, stage);
```
I believe this might have been a genuine bug.

source/MaterialXGenGlsl/GlslShaderGenerator.cpp
@@ -6,12 +6,6 @@
 #include <MaterialXGenGlsl/GlslShaderGenerator.h>
 
 #include <MaterialXGenGlsl/GlslSyntax.h>
-#include <MaterialXGenGlsl/Nodes/SurfaceNodeGlsl.h>
-#include <MaterialXGenGlsl/Nodes/LightNodeGlsl.h>
-#include <MaterialXGenGlsl/Nodes/LightCompoundNodeGlsl.h>
-#include <MaterialXGenGlsl/Nodes/LightShaderNodeGlsl.h>
-#include <MaterialXGenGlsl/Nodes/LightSamplerNodeGlsl.h>
-#include <MaterialXGenGlsl/Nodes/NumLightsNodeGlsl.h>
 
 #include <MaterialXGenShader/Nodes/MaterialNode.h>
 #include <MaterialXGenShader/Nodes/HwImageNode.h>
@@ -26,6 +20,12 @@
 #include <MaterialXGenShader/Nodes/HwFrameNode.h>
 #include <MaterialXGenShader/Nodes/HwTimeNode.h>
 #include <MaterialXGenShader/Nodes/HwViewDirectionNode.h>
+#include <MaterialXGenShader/Nodes/HwLightCompoundNode.h>
+#include <MaterialXGenShader/Nodes/HwLightNode.h>
+#include <MaterialXGenShader/Nodes/HwLightSamplerNode.h>
+#include <MaterialXGenShader/Nodes/HwLightShaderNode.h>
+#include <MaterialXGenShader/Nodes/HwNumLightsNode.h>
+#include <MaterialXGenShader/Nodes/HwSurfaceNode.h>
 
 MATERIALX_NAMESPACE_BEGIN
 
@@ -83,17 +83,17 @@ GlslShaderGenerator::GlslShaderGenerator(TypeSystemPtr typeSystem) :
     registerImplementation("IM_viewdirection_vector3_" + GlslShaderGenerator::TARGET, HwViewDirectionNode::create);
 
     // <!-- <surface> -->
-    registerImplementation("IM_surface_" + GlslShaderGenerator::TARGET, SurfaceNodeGlsl::create);
+    registerImplementation("IM_surface_" + GlslShaderGenerator::TARGET, HwSurfaceNode::create);
 
     // <!-- <light> -->
-    registerImplementation("IM_light_" + GlslShaderGenerator::TARGET, LightNodeGlsl::create);
+    registerImplementation("IM_light_" + GlslShaderGenerator::TARGET, HwLightNode::create);
 
     // <!-- <point_light> -->
-    registerImplementation("IM_point_light_" + GlslShaderGenerator::TARGET, LightShaderNodeGlsl::create);
+    registerImplementation("IM_point_light_" + GlslShaderGenerator::TARGET, HwLightShaderNode::create);
     // <!-- <directional_light> -->
-    registerImplementation("IM_directional_light_" + GlslShaderGenerator::TARGET, LightShaderNodeGlsl::create);
+    registerImplementation("IM_directional_light_" + GlslShaderGenerator::TARGET, HwLightShaderNode::create);
     // <!-- <spot_light> -->
-    registerImplementation("IM_spot_light_" + GlslShaderGenerator::TARGET, LightShaderNodeGlsl::create);
+    registerImplementation("IM_spot_light_" + GlslShaderGenerator::TARGET, HwLightShaderNode::create);
 
     // <!-- <ND_transformpoint> ->
     registerImplementation("IM_transformpoint_vector3_" + GlslShaderGenerator::TARGET, HwTransformPointNode::create);
@@ -118,8 +118,8 @@ GlslShaderGenerator::GlslShaderGenerator(TypeSystemPtr typeSystem) :
     // <!-- <surfacematerial> -->
     registerImplementation("IM_surfacematerial_" + GlslShaderGenerator::TARGET, MaterialNode::create);
 
-    _lightSamplingNodes.push_back(ShaderNode::create(nullptr, "numActiveLightSources", NumLightsNodeGlsl::create()));
-    _lightSamplingNodes.push_back(ShaderNode::create(nullptr, "sampleLightSource", LightSamplerNodeGlsl::create()));
+    _lightSamplingNodes.push_back(ShaderNode::create(nullptr, "numActiveLightSources", HwNumLightsNode::create()));
+    _lightSamplingNodes.push_back(ShaderNode::create(nullptr, "sampleLightSource", HwLightSamplerNode::create()));
 }
 
 ShaderPtr GlslShaderGenerator::generate(const string& name, ElementPtr element, GenContext& context) const
@@ -718,7 +718,7 @@ ShaderNodeImplPtr GlslShaderGenerator::getImplementation(const NodeDef& nodedef,
         // Use a compound implementation.
         if (outputType == Type::LIGHTSHADER)
         {
-            impl = LightCompoundNodeGlsl::create();
+            impl = HwLightCompoundNode::create();
         }
         else
         {

source/MaterialXGenMsl/MslShaderGenerator.cpp
@@ -6,12 +6,6 @@
 #include <MaterialXGenMsl/MslShaderGenerator.h>
 
 #include <MaterialXGenMsl/MslSyntax.h>
-#include <MaterialXGenMsl/Nodes/SurfaceNodeMsl.h>
-#include <MaterialXGenMsl/Nodes/LightNodeMsl.h>
-#include <MaterialXGenMsl/Nodes/LightCompoundNodeMsl.h>
-#include <MaterialXGenMsl/Nodes/LightShaderNodeMsl.h>
-#include <MaterialXGenMsl/Nodes/LightSamplerNodeMsl.h>
-#include <MaterialXGenMsl/Nodes/NumLightsNodeMsl.h>
 
 #include <MaterialXGenShader/Nodes/MaterialNode.h>
 #include <MaterialXGenShader/Nodes/HwImageNode.h>
@@ -26,6 +20,12 @@
 #include <MaterialXGenShader/Nodes/HwFrameNode.h>
 #include <MaterialXGenShader/Nodes/HwTimeNode.h>
 #include <MaterialXGenShader/Nodes/HwViewDirectionNode.h>
+#include <MaterialXGenShader/Nodes/HwLightCompoundNode.h>
+#include <MaterialXGenShader/Nodes/HwLightNode.h>
+#include <MaterialXGenShader/Nodes/HwLightSamplerNode.h>
+#include <MaterialXGenShader/Nodes/HwLightShaderNode.h>
+#include <MaterialXGenShader/Nodes/HwNumLightsNode.h>
+#include <MaterialXGenShader/Nodes/HwSurfaceNode.h>
 
 #include "MslResourceBindingContext.h"
 
@@ -88,17 +88,17 @@ MslShaderGenerator::MslShaderGenerator(TypeSystemPtr typeSystem) :
     registerImplementation("IM_viewdirection_vector3_" + MslShaderGenerator::TARGET, HwViewDirectionNode::create);
 
     // <!-- <surface> -->
-    registerImplementation("IM_surface_" + MslShaderGenerator::TARGET, SurfaceNodeMsl::create);
+    registerImplementation("IM_surface_" + MslShaderGenerator::TARGET, HwSurfaceNode::create);
 
     // <!-- <light> -->
-    registerImplementation("IM_light_" + MslShaderGenerator::TARGET, LightNodeMsl::create);
+    registerImplementation("IM_light_" + MslShaderGenerator::TARGET, HwLightNode::create);
 
     // <!-- <point_light> -->
-    registerImplementation("IM_point_light_" + MslShaderGenerator::TARGET, LightShaderNodeMsl::create);
+    registerImplementation("IM_point_light_" + MslShaderGenerator::TARGET, HwLightShaderNode::create);
     // <!-- <directional_light> -->
-    registerImplementation("IM_directional_light_" + MslShaderGenerator::TARGET, LightShaderNodeMsl::create);
+    registerImplementation("IM_directional_light_" + MslShaderGenerator::TARGET, HwLightShaderNode::create);
     // <!-- <spot_light> -->
-    registerImplementation("IM_spot_light_" + MslShaderGenerator::TARGET, LightShaderNodeMsl::create);
+    registerImplementation("IM_spot_light_" + MslShaderGenerator::TARGET, HwLightShaderNode::create);
 
     // <!-- <ND_transformpoint> ->
     registerImplementation("IM_transformpoint_vector3_" + MslShaderGenerator::TARGET, HwTransformPointNode::create);
@@ -123,8 +123,10 @@ MslShaderGenerator::MslShaderGenerator(TypeSystemPtr typeSystem) :
     // <!-- <surfacematerial> -->
     registerImplementation("IM_surfacematerial_" + MslShaderGenerator::TARGET, MaterialNode::create);
 
-    _lightSamplingNodes.push_back(ShaderNode::create(nullptr, "numActiveLightSources", NumLightsNodeMsl::create()));
-    _lightSamplingNodes.push_back(ShaderNode::create(nullptr, "sampleLightSource", LightSamplerNodeMsl::create()));
+    _lightSamplingNodes.push_back(ShaderNode::create(nullptr, "numActiveLightSources", HwNumLightsNode::create()));
+    _lightSamplingNodes.push_back(ShaderNode::create(nullptr, "sampleLightSource", HwLightSamplerNode::create()));
+
+    _tokenSubstitutions[HW::T_CLOSURE_DATA_CONSTRUCTOR] = "{closureType, L, V, N, P, occlusion}";
 }
 
 ShaderPtr MslShaderGenerator::generate(const string& name, ElementPtr element, GenContext& context) const
@@ -1233,7 +1235,7 @@ ShaderNodeImplPtr MslShaderGenerator::getImplementation(const NodeDef& nodedef, 
         // Use a compound implementation.
         if (outputType == Type::LIGHTSHADER)
         {
-            impl = LightCompoundNodeMsl::create();
+            impl = HwLightCompoundNode::create();
         }
         else
         {

source/MaterialXGenShader/Nodes/HwSurfaceNode.cpp
@@ -3,28 +3,27 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include <MaterialXGenMsl/Nodes/SurfaceNodeMsl.h>
-#include <MaterialXGenMsl/MslShaderGenerator.h>
+#include <MaterialXGenShader/Nodes/HwSurfaceNode.h>
 
 #include <MaterialXGenShader/Shader.h>
 #include <MaterialXGenShader/GenContext.h>
 
 MATERIALX_NAMESPACE_BEGIN
 
-SurfaceNodeMsl::SurfaceNodeMsl()
+HwSurfaceNode::HwSurfaceNode()
 {}
 
-ShaderNodeImplPtr SurfaceNodeMsl::create()
+ShaderNodeImplPtr HwSurfaceNode::create()
 {
-    return std::make_shared<SurfaceNodeMsl>();
+    return std::make_shared<HwSurfaceNode>();
 }
 
-void SurfaceNodeMsl::createVariables(const ShaderNode&, GenContext& context, Shader& shader) const
+void HwSurfaceNode::createVariables(const ShaderNode&, GenContext& context, Shader& shader) const
 {
     // TODO:
     // The surface shader needs position, normal, view position and light sources. We should solve this by adding some
     // dependency mechanism so this implementation can be set to depend on the HwPositionNode, HwNormalNode
-    // HwViewDirectionNode and LightNodeMsl nodes instead? This is where the MaterialX attribute "internalgeomprops"
+    // HwViewDirectionNode and LightNodeGlsl nodes instead? This is where the MaterialX attribute "internalgeomprops"
     // is needed.
     //
     ShaderStage& vs = shader.getStage(Stage::VERTEX);
@@ -39,13 +38,13 @@ void SurfaceNodeMsl::createVariables(const ShaderNode&, GenContext& context, Sha
 
     addStageUniform(HW::PRIVATE_UNIFORMS, Type::VECTOR3, HW::T_VIEW_POSITION, ps);
 
-    const MslShaderGenerator& shadergen = static_cast<const MslShaderGenerator&>(context.getShaderGenerator());
+    const HwShaderGenerator& shadergen = static_cast<const HwShaderGenerator&>(context.getShaderGenerator());
     shadergen.addStageLightingUniforms(context, ps);
 }
 
-void SurfaceNodeMsl::emitFunctionCall(const ShaderNode& node, GenContext& context, ShaderStage& stage) const
+void HwSurfaceNode::emitFunctionCall(const ShaderNode& node, GenContext& context, ShaderStage& stage) const
 {
-    const MslShaderGenerator& shadergen = static_cast<const MslShaderGenerator&>(context.getShaderGenerator());
+    const HwShaderGenerator& shadergen = static_cast<const HwShaderGenerator&>(context.getShaderGenerator());
 
     DEFINE_SHADER_STAGE(stage, Stage::VERTEX)
     {
@@ -61,7 +60,7 @@ void SurfaceNodeMsl::emitFunctionCall(const ShaderNode& node, GenContext& contex
         if (!normal->isEmitted())
         {
             normal->setEmitted();
-            shadergen.emitLine(prefix + normal->getVariable() + " = normalize((" + HW::T_WORLD_INVERSE_TRANSPOSE_MATRIX + " * float4(" + HW::T_IN_NORMAL + ", 0)).xyz)", stage);
+            shadergen.emitLine(prefix + normal->getVariable() + " = normalize(mx_matrix_mul(" + HW::T_WORLD_INVERSE_TRANSPOSE_MATRIX + ", vec4(" + HW::T_IN_NORMAL + ", 0)).xyz)", stage);
         }
         if (context.getOptions().hwAmbientOcclusion)
         {
@@ -87,10 +86,10 @@ void SurfaceNodeMsl::emitFunctionCall(const ShaderNode& node, GenContext& contex
 
         shadergen.emitScopeBegin(stage);
 
-        shadergen.emitLine("float3 N = normalize(" + prefix + HW::T_NORMAL_WORLD + ")", stage);
-        shadergen.emitLine("float3 V = normalize(" + HW::T_VIEW_POSITION + " - " + prefix + HW::T_POSITION_WORLD + ")", stage);
-        shadergen.emitLine("float3 P = " + prefix + HW::T_POSITION_WORLD, stage);
-        shadergen.emitLine("float3 L = float3(0,0,0);", stage);
+        shadergen.emitLine("vec3 N = normalize(" + prefix + HW::T_NORMAL_WORLD + ")", stage);
+        shadergen.emitLine("vec3 V = normalize(" + HW::T_VIEW_POSITION + " - " + prefix + HW::T_POSITION_WORLD + ")", stage);
+        shadergen.emitLine("vec3 P = " + prefix + HW::T_POSITION_WORLD, stage);
+        shadergen.emitLine("vec3 L = vec3(0,0,0);", stage);
         shadergen.emitLine("float occlusion = 1.0", stage);
         shadergen.emitLineBreak(stage);
 
@@ -112,10 +111,9 @@ void SurfaceNodeMsl::emitFunctionCall(const ShaderNode& node, GenContext& contex
             shadergen.emitComment("Shadow occlusion", stage);
             if (context.getOptions().hwShadowMap)
             {
-                shadergen.emitLine("float3 shadowCoord = (" + HW::T_SHADOW_MATRIX + " * float4(" + prefix + HW::T_POSITION_WORLD + ", 1.0)).xyz", stage);
-                shadergen.emitLine("shadowCoord.xy = shadowCoord.xy * 0.5 + 0.5", stage);
-
-                shadergen.emitLine("float2 shadowMoments = texture(" + HW::T_SHADOW_MAP + ", shadowCoord.xy).xy", stage);
+                shadergen.emitLine("vec3 shadowCoord = mx_matrix_mul(" + HW::T_SHADOW_MATRIX + ", vec4(" + prefix + HW::T_POSITION_WORLD + ", 1.0)).xyz", stage);
+                shadergen.emitLine("shadowCoord = shadowCoord * 0.5 + 0.5", stage);
+                shadergen.emitLine("vec2 shadowMoments = texture(" + HW::T_SHADOW_MAP + ", shadowCoord.xy).xy", stage);
                 shadergen.emitLine("occlusion = mx_variance_shadow_occlusion(shadowMoments, shadowCoord.z)", stage);
             }
             shadergen.emitLineBreak(stage);
@@ -129,7 +127,7 @@ void SurfaceNodeMsl::emitFunctionCall(const ShaderNode& node, GenContext& contex
             if (context.getOptions().hwAmbientOcclusion)
             {
                 ShaderPort* texcoord = vertexData[HW::T_TEXCOORD + "_0"];
-                shadergen.emitLine("float2 ambOccUv = mx_transform_uv(" + prefix + texcoord->getVariable() + ", float2(1.0), float2(0.0))", stage);
+                shadergen.emitLine("vec2 ambOccUv = mx_transform_uv(" + prefix + texcoord->getVariable() + ", vec2(1.0), vec2(0.0))", stage);
                 shadergen.emitLine("occlusion = mix(1.0, texture(" + HW::T_AMB_OCC_MAP + ", ambOccUv).x, " + HW::T_AMB_OCC_GAIN + ")", stage);
             }
             else
@@ -141,8 +139,9 @@ void SurfaceNodeMsl::emitFunctionCall(const ShaderNode& node, GenContext& contex
             shadergen.emitComment("Add environment contribution", stage);
             shadergen.emitScopeBegin(stage);
 
+            // indirect lighting
             if (bsdf->hasClassification(ShaderNode::Classification::BSDF_R)) {
-                shadergen.emitLine("ClosureData closureData = {CLOSURE_TYPE_INDIRECT, L, V, N, P, occlusion}", stage);
+                shadergen.emitLine("ClosureData closureData = makeClosureData(CLOSURE_TYPE_INDIRECT, L, V, N, P, occlusion)", stage);
                 shadergen.emitFunctionCall(*bsdf, context, stage);
             }
             else
@@ -168,7 +167,7 @@ void SurfaceNodeMsl::emitFunctionCall(const ShaderNode& node, GenContext& contex
             shadergen.emitScopeBegin(stage);
 
             if (edf->hasClassification(ShaderNode::Classification::EDF)) {
-                shadergen.emitLine("ClosureData closureData = {CLOSURE_TYPE_EMISSION, L, V, N, P, occlusion}", stage);
+                shadergen.emitLine("ClosureData closureData = makeClosureData(CLOSURE_TYPE_EMISSION, L, V, N, P, occlusion)", stage);
                 shadergen.emitFunctionCall(*edf, context, stage);
             }
             else
@@ -189,8 +188,8 @@ void SurfaceNodeMsl::emitFunctionCall(const ShaderNode& node, GenContext& contex
         if (const ShaderNode* bsdf = bsdfInput->getConnectedSibling())
         {
             shadergen.emitComment("Calculate the BSDF transmission for viewing direction", stage);
-            if (bsdf->hasClassification(ShaderNode::Classification::BSDF_T)) {
-                shadergen.emitLine("ClosureData closureData = {CLOSURE_TYPE_TRANSMISSION, L, V, N, P, occlusion}", stage);
+            if (bsdf->hasClassification(ShaderNode::Classification::BSDF_T) || bsdf->hasClassification(ShaderNode::Classification::VDF)) {
+                shadergen.emitLine("ClosureData closureData = makeClosureData(CLOSURE_TYPE_TRANSMISSION, L, V, N, P, occlusion)", stage);
                 shadergen.emitFunctionCall(*bsdf, context, stage);
             }
             else
@@ -213,7 +212,7 @@ void SurfaceNodeMsl::emitFunctionCall(const ShaderNode& node, GenContext& contex
             shadergen.emitComment("Compute and apply surface opacity", stage);
             shadergen.emitScopeBegin(stage);
             shadergen.emitLine(outColor + " *= surfaceOpacity", stage);
-            shadergen.emitLine(outTransparency + " = mix(float3(1.0), " + outTransparency + ", surfaceOpacity)", stage);
+            shadergen.emitLine(outTransparency + " = mix(vec3(1.0), " + outTransparency + ", surfaceOpacity)", stage);
             shadergen.emitScopeEnd(stage);
         }
 
@@ -222,14 +221,14 @@ void SurfaceNodeMsl::emitFunctionCall(const ShaderNode& node, GenContext& contex
     }
 }
 
-void SurfaceNodeMsl::emitLightLoop(const ShaderNode& node, GenContext& context, ShaderStage& stage, const string& outColor) const
+void HwSurfaceNode::emitLightLoop(const ShaderNode& node, GenContext& context, ShaderStage& stage, const string& outColor) const
 {
     //
     // Generate Light loop if requested
     //
     if (context.getOptions().hwMaxActiveLightSources > 0)
     {
-        const MslShaderGenerator& shadergen = static_cast<const MslShaderGenerator&>(context.getShaderGenerator());
+        const HwShaderGenerator& shadergen = static_cast<const HwShaderGenerator&>(context.getShaderGenerator());
         const VariableBlock& vertexData = stage.getInputBlock(HW::VERTEX_DATA);
         const string prefix = shadergen.getVertexDataPrefix(vertexData);
 
@@ -249,7 +248,7 @@ void SurfaceNodeMsl::emitLightLoop(const ShaderNode& node, GenContext& context, 
 
         shadergen.emitComment("Calculate the BSDF response for this light source", stage);
         if (bsdf->hasClassification(ShaderNode::Classification::BSDF_R)) {
-            shadergen.emitLine("ClosureData closureData = {CLOSURE_TYPE_REFLECTION, L, V, N, P, occlusion}", stage);
+            shadergen.emitLine("ClosureData closureData = makeClosureData(CLOSURE_TYPE_REFLECTION, L, V, N, P, occlusion)", stage);
             shadergen.emitFunctionCall(*bsdf, context, stage);
         }
         else

libraries/pbrlib/genglsl/lib/mx_closure_type.glsl
@@ -15,3 +15,8 @@ struct ClosureData {
     vec3 P;
     float occlusion;
 };
+
+ClosureData makeClosureData(int closureType, vec3 L, vec3 V, vec3 N, vec3 P, float occlusion)
+{
+    return $closureDataConstructor;
+}

source/MaterialXGenGlsl/Nodes/LightCompoundNodeGlsl.cpp
@@ -1,92 +1,0 @@
-//
-// Copyright Contributors to the MaterialX Project
-// SPDX-License-Identifier: Apache-2.0
-//
-
-#include <MaterialXGenGlsl/Nodes/LightCompoundNodeGlsl.h>
-#include <MaterialXGenGlsl/GlslShaderGenerator.h>
-
-#include <MaterialXGenShader/Util.h>
-
-MATERIALX_NAMESPACE_BEGIN
-
-LightCompoundNodeGlsl::LightCompoundNodeGlsl() :
-    _lightUniforms(HW::LIGHT_DATA, EMPTY_STRING)
-{
-}
-
-ShaderNodeImplPtr LightCompoundNodeGlsl::create()
-{
-    return std::make_shared<LightCompoundNodeGlsl>();
-}
-
-void LightCompoundNodeGlsl::initialize(const InterfaceElement& element, GenContext& context)
-{
-    CompoundNode::initialize(element, context);
-
-    // Store light uniforms for all inputs on the interface
-    const NodeGraph& graph = static_cast<const NodeGraph&>(element);
-    NodeDefPtr nodeDef = graph.getNodeDef();
-    for (InputPtr input : nodeDef->getActiveInputs())
-    {
-        const TypeDesc type = context.getTypeDesc(input->getType());
-        _lightUniforms.add(type, input->getName());
-    }
-}
-
-void LightCompoundNodeGlsl::createVariables(const ShaderNode&, GenContext& context, Shader& shader) const
-{
-    // Create variables for all child nodes
-    for (ShaderNode* childNode : _rootGraph->getNodes())
-    {
-        childNode->getImplementation().createVariables(*childNode, context, shader);
-    }
-
-    ShaderStage& ps = shader.getStage(Stage::PIXEL);
-    VariableBlock& lightData = ps.getUniformBlock(HW::LIGHT_DATA);
-
-    // Create all light uniforms
-    for (size_t i = 0; i < _lightUniforms.size(); ++i)
-    {
-        ShaderPort* u = const_cast<ShaderPort*>(_lightUniforms[i]);
-        lightData.add(u->getSelf());
-    }
-
-    const GlslShaderGenerator& shadergen = static_cast<const GlslShaderGenerator&>(context.getShaderGenerator());
-    shadergen.addStageLightingUniforms(context, ps);
-}
-
-void LightCompoundNodeGlsl::emitFunctionDefinition(const ShaderNode& /*node*/, GenContext& context, ShaderStage& stage) const
-{
-    DEFINE_SHADER_STAGE(stage, Stage::PIXEL)
-    {
-        const GlslShaderGenerator& shadergen = static_cast<const GlslShaderGenerator&>(context.getShaderGenerator());
-
-        // Emit functions for all child nodes
-        shadergen.emitFunctionDefinitions(*_rootGraph, context, stage);
-
-        shadergen.emitLine("void " + _functionName + "(LightData light, vec3 position, out lightshader result)", stage, false);
-
-        shadergen.emitFunctionBodyBegin(*_rootGraph, context, stage);
-
-        // Emit all texturing nodes. These are inputs to any
-        // closure/shader nodes and need to be emitted first.
-        shadergen.emitFunctionCalls(*_rootGraph, context, stage, ShaderNode::Classification::TEXTURE);
-
-        shadergen.emitLine("ClosureData closureData = ClosureData(CLOSURE_TYPE_EMISSION, vec3(0), -L, light.direction, vec3(0), 0)", stage);
-        shadergen.emitFunctionCalls(*_rootGraph, context, stage, ShaderNode::Classification::SHADER | ShaderNode::Classification::LIGHT);
-
-        shadergen.emitFunctionBodyEnd(*_rootGraph, context, stage);
-    }
-}
-
-void LightCompoundNodeGlsl::emitFunctionCall(const ShaderNode&, GenContext& context, ShaderStage& stage) const
-{
-    DEFINE_SHADER_STAGE(stage, Stage::PIXEL)
-    {
-        const ShaderGenerator& shadergen = context.getShaderGenerator();
-        shadergen.emitLine(_functionName + "(light, position, result)", stage);
-    }
-}
-
-MATERIALX_NAMESPACE_END

source/MaterialXGenGlsl/Nodes/LightCompoundNodeGlsl.h
@@ -1,41 +1,0 @@
-//
-// Copyright Contributors to the MaterialX Project
-// SPDX-License-Identifier: Apache-2.0
-//
-
-#ifndef MATERIALX_LIGHTCOMPOUNDNODEGLSL_H
-#define MATERIALX_LIGHTCOMPOUNDNODEGLSL_H
-
-#include <MaterialXGenGlsl/Export.h>
-
-#include <MaterialXGenShader/Nodes/CompoundNode.h>
-#include <MaterialXGenShader/Shader.h>
-#include <MaterialXGenShader/GenContext.h>
-
-MATERIALX_NAMESPACE_BEGIN
-
-class GlslShaderGenerator;
-
-/// LightCompound node implementation for GLSL
-class MX_GENGLSL_API LightCompoundNodeGlsl : public CompoundNode
-{
-  public:
-    LightCompoundNodeGlsl();
-
-    static ShaderNodeImplPtr create();
-
-    void initialize(const InterfaceElement& element, GenContext& context) override;
-
-    void createVariables(const ShaderNode& node, GenContext& context, Shader& shader) const override;
-
-    void emitFunctionDefinition(const ShaderNode& node, GenContext& context, ShaderStage& stage) const override;
-
-    void emitFunctionCall(const ShaderNode& node, GenContext& context, ShaderStage& stage) const override;
-
-  protected:
-    VariableBlock _lightUniforms;
-};
-
-MATERIALX_NAMESPACE_END
-
-#endif

commit 3a92003aa2b4865e6ebe600c48fb3ee360c02b95

Restore platform-specific shadow code in HW generation (#2607)
A small bug was introduced in #2555 causing the texture coordinates used in Metal when applying the shadow map to be incorrect, leading to a darker image.

This was originally noted by @ashwinbhat [here](https://academysoftwarefdn.slack.com/archives/C0230LWBE2X/p1759876675993069).

libraries/pbrlib/genglsl/lib/mx_shadow_platform.glsl
@@ -1,0 +1,13 @@
+
+float mx_shadow_occlusion(
+    $texSamplerSignature,
+    mat4 shadow_matrix,
+    vec3 world_position
+)
+{
+    vec4 shadowCoord4 = mx_matrix_mul(shadow_matrix, vec4(world_position, 1.0));
+    vec3 shadowCoord = shadowCoord4.xyz / shadowCoord4.w;
+    shadowCoord = shadowCoord * 0.5 + 0.5;
+    vec2 shadowMoments = texture($texSamplerSampler2D, shadowCoord.xy).xy;
+    return  mx_variance_shadow_occlusion(shadowMoments, shadowCoord.z);
+}

libraries/pbrlib/genmsl/lib/mx_lighting_platform.metal
@@ -1,0 +1,13 @@
+
+float mx_shadow_occlusion(
+    MetalTexture shadow_map_tex,
+    float4x4 shadow_matrix,
+    float3 world_position
+)
+{
+    float4 shadowCoord4 = mx_matrix_mul(shadow_matrix, float4(world_position, 1.0));
+    float3 shadowCoord = shadowCoord4.xyz / shadowCoord4.w;
+    shadowCoord.xy = shadowCoord.xy * 0.5 + 0.5;
+    float2 shadowMoments = texture(shadow_map_tex, shadowCoord.xy).xy;
+    return mx_variance_shadow_occlusion(shadowMoments, shadowCoord.z);
+}

source/MaterialXGenGlsl/GlslShaderGenerator.cpp
@@ -434,6 +434,7 @@ void GlslShaderGenerator::emitPixelStage(const ShaderGraph& graph, GenContext& c
     if (shadowing)
     {
         emitLibraryInclude("pbrlib/genglsl/lib/mx_shadow.glsl", context, stage);
+        emitLibraryInclude("pbrlib/genglsl/lib/mx_shadow_platform.glsl", context, stage);
     }
 
     // Emit directional albedo table code.

source/MaterialXGenHw/Nodes/HwSurfaceNode.cpp
@@ -114,11 +114,7 @@ void HwSurfaceNode::emitFunctionCall(const ShaderNode& node, GenContext& context
             shadergen.emitComment("Shadow occlusion", stage);
             if (context.getOptions().hwShadowMap)
             {
-                shadergen.emitLine("vec4 shadowCoord4 = mx_matrix_mul(" + HW::T_SHADOW_MATRIX + ", vec4(" + prefix + HW::T_POSITION_WORLD + ", 1.0))", stage);
-                shadergen.emitLine("vec3 shadowCoord = shadowCoord4.xyz / shadowCoord4.w;", stage);
-                shadergen.emitLine("shadowCoord = shadowCoord * 0.5 + 0.5", stage);
-                shadergen.emitLine("vec2 shadowMoments = texture(" + HW::T_SHADOW_MAP + ", shadowCoord.xy).xy", stage);
-                shadergen.emitLine("occlusion = mx_variance_shadow_occlusion(shadowMoments, shadowCoord.z)", stage);
+                shadergen.emitLine("occlusion = mx_shadow_occlusion(" + HW::T_SHADOW_MAP + ", " + HW::T_SHADOW_MATRIX + ", " + prefix + HW::T_POSITION_WORLD + ")", stage);
             }
             shadergen.emitLineBreak(stage);
 

source/MaterialXGenMsl/MslShaderGenerator.cpp
@@ -930,6 +930,7 @@ void MslShaderGenerator::emitPixelStage(const ShaderGraph& graph, GenContext& co
         if (shadowing)
         {
             emitLibraryInclude("pbrlib/genglsl/lib/mx_shadow.glsl", context, stage);
+            emitLibraryInclude("pbrlib/genmsl/lib/mx_shadow_platform.metal", context, stage);
         }
 
         // Emit directional albedo table code.