0c1ed1e1dbc2a48ad85e39bd67fc38d89ffdd2d6
Bernard Kwok <kwokcb@gmail.com>
2025-11-03 11:22:44 -0500

    Fixes for MaterialX format checker (#2647)
    ## Changes
    
    The utility  `mx_format.py` was not traversing to sub-folders so when applied to `libraries` it would miss some files.
    This fixes some files which had invalid XML (unescaped characters) as `PugiXML` will "fix" this on write.
    
    Two additional options have been added:
    - Check for XML syntax validity
    - MaterialX validation checking
    Both are off by default.
    
    ## Results
    
    - Ran this again `libraries` folder. Changes are mostly cosmetic except for the mentioned fixes to missed files.
    - Ran this against `resources` folder. Found a file which had duplicate elements. Fixed manually.

--- python/Scripts/mxformat.py
+++ python/Scripts/mxformat.py
@@ -6,21 +6,31 @@ the documents to the latest version of the standard.
 
 import argparse
 import os
+import xml.etree.ElementTree as ET
 
 import MaterialX as mx
 
+def is_well_formed(xml_string):
+    error = ''
+    try:
+        ET.fromstring(xml_string)  
+    except ET.ParseError as e:
+        error = str(e)
+
 def main():
     parser = argparse.ArgumentParser(description="Reformat a folder of MaterialX documents in place.")
-    parser.add_argument("--yes", dest="yes", action="store_true", help="Proceed without asking for confirmation from the user.")
-    parser.add_argument('--upgrade', dest='upgrade', action="store_true", help='Upgrade documents to the latest version of the standard.')
+    parser.add_argument('-y', '--yes', dest='yes', action="store_true", help="Proceed without asking for confirmation from the user.")
+    parser.add_argument('-u', '--upgrade', dest='upgrade', action="store_true", help='Upgrade documents to the latest version of the standard.')
+    parser.add_argument('-v', '--validate', dest='validate', action="store_true", help='Perform MaterialX validation on documents after reformatting.')
+    parser.add_argument('-x', '--xml_syntax', dest='xml_syntax', action="store_true", help='Check XML syntax after reformatting.')
     parser.add_argument(dest="inputFolder", help="An input folder to scan for MaterialX documents.")
     opts = parser.parse_args()
 
     validDocs = dict()
     for root, dirs, files in os.walk(opts.inputFolder):
-        for file in files:
-            if file.endswith('.mtlx'):
-                filename = os.path.join(root, file)
+        for filename in files:
+            fullpath = os.path.join(root, filename)
+            if fullpath.endswith('.mtlx'):
                 doc = mx.createDocument()
                 try:
                     readOptions = mx.XmlReadOptions()
@@ -28,11 +38,11 @@ def main():
                     readOptions.readNewlines = True    
                     readOptions.upgradeVersion = opts.upgrade
                     try:
-                        mx.readFromXmlFile(doc, filename, mx.FileSearchPath(), readOptions)
+                        mx.readFromXmlFile(doc, fullpath, mx.FileSearchPath(), readOptions)
                     except Exception as err:
-                        print('Skipping "' + file + '" due to exception: ' + str(err))
+                        print('Skipping "' + filename + '" due to exception: ' + str(err))
                         continue
-                    validDocs[filename] = doc
+                    validDocs[fullpath] = doc
                 except mx.Exception:
                     pass
 
@@ -53,7 +63,22 @@ def main():
         if answer != 'y' and answer != 'Y':
             return
 
+    validate = opts.validate
+    if validate:
+        print(f'- Validate documents')
+    xml_syntax = opts.xml_syntax
+    if xml_syntax:
+        print(f'- Check XML syntax')
     for (filename, doc) in validDocs.items():
+        if xml_syntax:
+            xml_string = mx.writeToXmlString(doc)
+            errors = is_well_formed(xml_string)
+            if errors:
+                print(f'- Warning: Document {filename} is not well-formed XML: {errors}') 
+        if validate:
+            is_valid, errors = doc.validate()
+            if not is_valid:
+                print(f'- Warning: Document {filename} is invalid. Errors {errors}.')
         mx.writeToXmlFile(doc, filename)
 
     if opts.upgrade:
