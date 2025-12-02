dde34944c75941388b51b3630e700ee656707dc6
Lee Kerley <ld_kerley@icloud.com>
2025-12-02 11:48:15 -0800

    Implement constant wrap mode in shader source to ensure portability across languages.

--- libraries/stdlib/genglsl/mx_image_vector2.glsl
+++ libraries/stdlib/genglsl/mx_image_vector2.glsl
@@ -3,5 +3,13 @@
 void mx_image_vector2($texSamplerSignature, int layer, vec2 defaultval, vec2 texcoord, int uaddressmode, int vaddressmode, int filtertype, int framerange, int frameoffset, int frameendaction, vec2 uv_scale, vec2 uv_offset, out vec2 result)
 {
     vec2 uv = mx_transform_uv(texcoord, uv_scale, uv_offset);
+
+    // "constant" wrap mode directly implemented in shader source because MSL and GLSL implementations don't align
+    if ((uaddressmode == 0 && (uv[0] < 0 || uv[0] > 1)) || (vaddressmode == 0 && (uv[1] < 0 || uv[1] > 1)))
+    {
+        result = defaultval;
+        return;
+    }
+
     result = texture($texSamplerSampler2D, uv).rg;
 }
