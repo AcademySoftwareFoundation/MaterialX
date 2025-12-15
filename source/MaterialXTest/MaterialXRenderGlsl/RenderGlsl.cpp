47852e296aefdfabe3c61e99efc5950b78131ffa
Lee Kerley <ld_kerley@icloud.com>
2025-12-15 13:48:46 -0800

    Update OSL render testing to use sphere.obj, and remove texture coordinate flipping in GLSL/MSL render test to ensure the renders align.
    This may mean the UVs are flipped - and we will re-evaluate this after this is merged.
    Remove local tangent/bitangent nodes now we're using OBJ
    Update OSL render template to write absolute paths to make debugging externally easier.

--- source/MaterialXTest/MaterialXRenderGlsl/RenderGlsl.cpp
+++ source/MaterialXTest/MaterialXRenderGlsl/RenderGlsl.cpp
@@ -262,11 +262,8 @@ bool GlslShaderRenderTester::runRenderer(const std::string& shaderName,
 
                 if (!geomHandler->hasGeometry(geomPath))
                 {
-                    // For test sphere and plane geometry perform a V-flip of texture coordinates.
-                    const std::string baseName = geomPath.getBaseName();
-                    bool texcoordVerticalFlip = baseName == "sphere.obj" || baseName == "plane.obj";
                     geomHandler->clearGeometry();
-                    geomHandler->loadGeometry(geomPath, texcoordVerticalFlip);
+                    geomHandler->loadGeometry(geomPath);
                     for (mx::MeshPtr mesh : geomHandler->getMeshes())
                     {
                         addAdditionalTestStreams(mesh);
