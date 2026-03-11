1e5ff1db52be260e9fc18b26c0c5de4bc97a1c53
jreichel-nvidia <43646584+jreichel-nvidia@users.noreply.github.com>
2026-03-11 18:07:55 +0100

    Improve material name handling in shader generation (#2804)
    Convert names of shader graph nodes early to identifiers to increase the chance of a name without suffixes. This allows to render all materials from the testsuite and examples with MDL without the need to skip some of them.

--- source/MaterialXGenShader/ShaderGraph.cpp
+++ source/MaterialXGenShader/ShaderGraph.cpp
@@ -18,10 +18,12 @@ MATERIALX_NAMESPACE_BEGIN
 // ShaderGraph methods
 //
 
-ShaderGraph::ShaderGraph(const ShaderGraph* parent, const string& name, ConstDocumentPtr document) :
-    ShaderNode(parent, name),
+ShaderGraph::ShaderGraph(const ShaderGraph* parent, const string& name, ConstDocumentPtr document,
+                         GenContext& context)
+  : ShaderNode(parent, name),
     _document(document)
 {
+    context.getShaderGenerator().getSyntax().makeIdentifier(_name, getIdentifierMap());
 }
 
 void ShaderGraph::addInputSockets(const InterfaceElement& elem, GenContext& context)
@@ -436,7 +438,7 @@ ShaderGraphPtr ShaderGraph::create(const ShaderGraph* parent, const NodeGraph& n
 
     string graphName = nodeGraph.getName();
     context.getShaderGenerator().getSyntax().makeValidName(graphName);
-    ShaderGraphPtr graph = std::make_shared<ShaderGraph>(parent, graphName, nodeGraph.getDocument());
+    ShaderGraphPtr graph = std::make_shared<ShaderGraph>(parent, graphName, nodeGraph.getDocument(), context);
 
     // Clear classification
     graph->_classification = 0;
@@ -495,7 +497,7 @@ ShaderGraphPtr ShaderGraph::create(const ShaderGraph* parent, const string& name
             throw ExceptionShaderGenError("Given output '" + output->getName() + "' has no interface valid for shader generation");
         }
 
-        graph = std::make_shared<ShaderGraph>(parent, name, element->getDocument());
+        graph = std::make_shared<ShaderGraph>(parent, name, element->getDocument(), context);
 
         // Clear classification
         graph->_classification = 0;
@@ -531,7 +533,7 @@ ShaderGraphPtr ShaderGraph::create(const ShaderGraph* parent, const string& name
             throw ExceptionShaderGenError("Could not find a nodedef for node '" + node->getName() + "'");
         }
 
-        graph = std::make_shared<ShaderGraph>(parent, name, element->getDocument());
+        graph = std::make_shared<ShaderGraph>(parent, name, element->getDocument(), context);
 
         // Create input sockets
         graph->addInputSockets(*nodeDef, context);
