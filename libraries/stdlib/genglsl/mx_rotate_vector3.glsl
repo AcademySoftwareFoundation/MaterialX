c22f93734d1efd8c6470ac2ef587ec3b5f03fd4d
Ben Houston <neuralsoft@gmail.com>
2026-05-06 22:04:32 -0400

    replace matrix44 construction for vector rotation with more efficient Rodgrides formula

--- libraries/stdlib/genglsl/mx_rotate_vector3.glsl
+++ libraries/stdlib/genglsl/mx_rotate_vector3.glsl
@@ -1,19 +1,10 @@
-mat4 mx_rotationMatrix(vec3 axis, float angle)
-{
-    axis = normalize(axis);
-    float s = mx_sin(angle);
-    float c = mx_cos(angle);
-    float oc = 1.0 - c;
-
-    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
-                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
-                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
-                0.0,                                0.0,                                0.0,                                1.0);
-}
-
 void mx_rotate_vector3(vec3 _in, float amount, vec3 axis, out vec3 result)
 {
+    // https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
+    axis = normalize(axis);
     float rotationRadians = mx_radians(amount);
-    mat4 m = mx_rotationMatrix(axis, rotationRadians);
-    result = mx_matrix_mul(m, vec4(_in, 1.0)).xyz;
+    float s = mx_sin(rotationRadians);
+    float c = mx_cos(rotationRadians);
+    float oc = 1.0 - c;
+    result = _in * c + cross(_in, axis) * s + axis * dot(axis, _in) * oc;
 }
