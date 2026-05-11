2010ac35c4097189ff3874b172576027e7711267
Ben Houston <neuralsoft@gmail.com>
2026-05-11 10:03:50 -0400

    explain why the paramers are flipped the first cross in Rodrigues' formula.

--- libraries/stdlib/genglsl/mx_rotate_vector3.glsl
+++ libraries/stdlib/genglsl/mx_rotate_vector3.glsl
@@ -1,6 +1,9 @@
 void mx_rotate_vector3(vec3 _in, float amount, vec3 axis, out vec3 result)
 {
-    // https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
+    // based on https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
+    // but the code in the Wikipedia article is for v' = M * v, where as 
+    // MaterialX is v' = v * M, thus the order of parameters into the first cross are reversed
+
     axis = normalize(axis);
     float rotationRadians = mx_radians(amount);
     float s = mx_sin(rotationRadians);
