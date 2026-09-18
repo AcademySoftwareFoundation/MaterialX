3d153233ee42cbd07a2390426474a309be640b3b
Savelii Pototskii <savelii.pototskii@gmail.com>
2026-09-19 02:41:48 +0500

    Fix edge color clamping in `artistic_ior` (#3078)
    This fixes the issue related to the green circle on some materials in #3004.
    
    Three shaded backends were changed and tested: GLSL, OSL, and MDL.

--- libraries/pbrlib/genglsl/mx_artistic_ior.glsl
+++ libraries/pbrlib/genglsl/mx_artistic_ior.glsl
@@ -4,10 +4,11 @@ void mx_artistic_ior(vec3 reflectivity, vec3 edge_color, out vec3 ior, out vec3 
     // http://jcgt.org/published/0003/04/03/paper.pdf
 
     vec3 r = clamp(reflectivity, 0.0, 0.99);
+    vec3 g = clamp(edge_color, 0.0, 1.0);
     vec3 r_sqrt = sqrt(r);
     vec3 n_min = (1.0 - r) / (1.0 + r);
     vec3 n_max = (1.0 + r_sqrt) / (1.0 - r_sqrt);
-    ior = mix(n_max, n_min, edge_color);
+    ior = mix(n_max, n_min, g);
 
     vec3 np1 = ior + 1.0;
     vec3 nm1 = ior - 1.0;
